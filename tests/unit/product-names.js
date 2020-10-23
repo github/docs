e08bc978fb53b70b42a4905eebd89d2acbd78fd8
Nick Schonning <nschonni@gmail.com>
2020-10-22 22:16:55 -0400

    typo: frienly -> friendly

--- tests/unit/product-names.js
+++ tests/unit/product-names.js
@@ -1,7 +1,7 @@
 const productNames = require('../../lib/product-names')
 
 describe('productNames module', () => {
-  test('is an object with product codes as keys and human-frienly names as values', () => {
+  test('is an object with product codes as keys and human-friendly names as values', () => {
     expect(productNames.dotcom).toBe('GitHub.com')
     expect(productNames['2.15']).toBe('Enterprise Server 2.15')
     expect(productNames['2.10']).toBe('Enterprise Server 2.10')
