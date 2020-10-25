ace1db3693c3b02d0ab06f48c58e346f234b2b36
Nick Schonning <nschonni@gmail.com>
2020-10-25 17:34:16 -0400

    chore: Upgrade actions/script to v3.0.0

--- .github/allowed-actions.js
+++ .github/allowed-actions.js
@@ -7,10 +7,7 @@ module.exports = [
   'actions/cache@70655ec8323daeeaa7ef06d7c56e1b9191396cbe',
   'actions/cache@d1255ad9362389eac595a9ae406b8e8cb3331f16',
   'actions/checkout@a81bbbf8298c0fa03ea29cdc473d45769f953675',
-  'actions/github-script@5d03ada4b0a753e9460b312e61cc4f8fdeacf163',
-  'actions/github-script@6e5ee1dc1cb3740e5e5e76ad668e3f526edbfe45',
-  'actions/github-script@44b873bc975058192f5279ebe7579496381f575d',
-  'actions/github-script@626af12fe9a53dc2972b48385e7fe7dec79145c9',
+  'actions/github-script@626af12fe9a53dc2972b48385e7fe7dec79145c9', //actions/script@v3.0.0
   'actions/labeler@5f867a63be70efff62b767459b009290364495eb',
   'actions/setup-node@56899e050abffc08c2b3b61f3ec6a79a9dc3223d',
   'actions/setup-ruby@5f29a1cd8dfebf420691c4c9a0e832e2fae5a526',
