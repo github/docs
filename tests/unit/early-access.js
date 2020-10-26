1e9a3196479f2f327290129ad06cb0cb25289f5d
Sarah Schneider <sarahs@github.com>
2020-10-26 09:52:16 -0400

    conditional test of early access in actions only

--- tests/unit/early-access.js
+++ tests/unit/early-access.js
@@ -1,11 +1,13 @@
-// TODO test ea-config
-
 const fs = require('fs')
 const path = require('path')
 
-// TODO this should not fail if run locally without cloning early-access
+const testViaActionsOnly = process.env.GITHUB_ACTIONS ? test : test.skip
+
+// TODO test ea-config
+
+// TODO this should only run locally
 describe('cloning content/early-access', () => {
-  test('the content directory exists', async () => {
+  testViaActionsOnly('the content directory exists', async () => {
     const eaContentDir = path.join(process.cwd(), 'content/early-access-test')
     expect(fs.existsSync(eaContentDir)).toBe(true)
   })
