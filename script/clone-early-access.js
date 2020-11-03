d00f43758cae80d924c043a04829e2c39c905db2
James M. Greene <jamesmgreene@github.com>
2020-11-03 10:59:08 -0600

    Remove debugging statements

--- script/clone-early-access.js
+++ script/clone-early-access.js
@@ -31,11 +31,6 @@ const earlyAccessDir = 'early-access-test'
 const earlyAccessFullRepo = `https://x-access-token:${DOCUBOT_REPO_PAT}@github.com/${earlyAccessOwner}/${earlyAccessDir}`
 const earlyAccessContentDir = path.join(process.cwd(), 'content', earlyAccessDir)
 
-console.log(`PAT 0-8: ${DOCUBOT_REPO_PAT.slice(0, 8)}`)
-console.log(`PAT 8-16: ${DOCUBOT_REPO_PAT.slice(8, 16)}`)
-console.log(`PAT 16-24: ${DOCUBOT_REPO_PAT.slice(16, 24)}`)
-console.log(`PAT 24+: ${DOCUBOT_REPO_PAT.slice(24)}`)
-
 // Production vs. staging environment
 // TODO test that this works as expected
 const environment = HEROKU_PRODUCTION_APP ? 'production' : 'staging'
