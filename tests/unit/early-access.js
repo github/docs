4fac3decec3dcef5413199d8bdf8527685a09827
James M. Greene <jamesmgreene@github.com>
2020-11-09 23:04:24 -0600

    Comment out premature tests

--- tests/unit/early-access.js
+++ tests/unit/early-access.js
@@ -3,22 +3,23 @@ const path = require('path')
 
 const testViaActionsOnly = process.env.GITHUB_ACTIONS ? test : test.skip
 
+//
 // TODO test ea-config
+//
 
-// TODO this should only run locally
 describe('cloning early-access', () => {
-  testViaActionsOnly('the assets directory exists', async () => {
-    const eaContentDir = path.join(process.cwd(), 'assets/early-access')
-    expect(fs.existsSync(eaContentDir)).toBe(true)
-  })
-
   testViaActionsOnly('the content directory exists', async () => {
     const eaContentDir = path.join(process.cwd(), 'content/early-access')
     expect(fs.existsSync(eaContentDir)).toBe(true)
   })
 
-  testViaActionsOnly('the data directory exists', async () => {
-    const eaContentDir = path.join(process.cwd(), 'data/early-access')
-    expect(fs.existsSync(eaContentDir)).toBe(true)
-  })
+  // testViaActionsOnly('the data directory exists', async () => {
+  //   const eaContentDir = path.join(process.cwd(), 'data/early-access')
+  //   expect(fs.existsSync(eaContentDir)).toBe(true)
+  // })
+
+  // testViaActionsOnly('the assets directory exists', async () => {
+  //   const eaContentDir = path.join(process.cwd(), 'assets/early-access')
+  //   expect(fs.existsSync(eaContentDir)).toBe(true)
+  // })
 })
