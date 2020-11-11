1b3af6e8c5954201094d1c526634f3d07bd494c7
James M. Greene <jamesmgreene@github.com>
2020-11-11 00:28:12 -0600

    Add Early Access context via middleware to show list of pages

---
+++ middleware/early-access-context.js
@@ -0,0 +1,26 @@
+module.exports = function earlyAccessContext (req, res, next) {
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(403).end()
+  }
+
+  const earlyAccessPages = req.context.pages
+    .filter(page => page.hidden)
+    .sort((a, b) => b.relativePath.localeCompare(a.relativePath))
+
+  let urls = []
+  earlyAccessPages.forEach(page => {
+    const pageUrls = page.permalinks.map(permalink => permalink.href)
+    urls = urls.concat(pageUrls)
+  })
+
+  const earlyAccessPageLinks = `
+${urls.map(url => `- [${url}](${url})`).join('\n')}
+`
+
+  console.log({ earlyAccessPageLinks })
+
+  // Add to the rendering context
+  req.context.earlyAccessPageLinks = earlyAccessPageLinks
+
+  return next()
+}
