b422034fa769df57e5fc9e25ad4550591907d5b9
Nick Schonning <nschonni@gmail.com>
2020-11-13 17:27:08 -0500

    chore: Move standard to ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true
+  },
+  parser: 'babel-eslint',
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+  },
+  overrides: [
+    {
+      files: [
+        '**/tests/**/*.js'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
