04dff854c256c0efcb3d3806c3bb26ced507e10f
James M. Greene <jamesmgreene@github.com>
2020-11-17 11:50:18 -0600

    Include other actions-only EA-cloning tests now

--- tests/unit/early-access.js
+++ tests/unit/early-access.js
@@ -5,23 +5,19 @@ const { GITHUB_ACTIONS, GITHUB_REPOSITORY } = process.env
 const runningActionsOnInternalRepo = GITHUB_ACTIONS === 'true' && GITHUB_REPOSITORY === 'github/docs-internal'
 const testViaActionsOnly = runningActionsOnInternalRepo ? test : test.skip
 
-//
-// TODO test ea-config
-//
-
 describe('cloning early-access', () => {
   testViaActionsOnly('the content directory exists', async () => {
     const eaContentDir = path.join(process.cwd(), 'content/early-access')
     expect(fs.existsSync(eaContentDir)).toBe(true)
   })
 
-  // testViaActionsOnly('the data directory exists', async () => {
-  //   const eaContentDir = path.join(process.cwd(), 'data/early-access')
-  //   expect(fs.existsSync(eaContentDir)).toBe(true)
-  // })
+  testViaActionsOnly('the data directory exists', async () => {
+    const eaContentDir = path.join(process.cwd(), 'data/early-access')
+    expect(fs.existsSync(eaContentDir)).toBe(true)
+  })
 
-  // testViaActionsOnly('the assets directory exists', async () => {
-  //   const eaContentDir = path.join(process.cwd(), 'assets/early-access')
-  //   expect(fs.existsSync(eaContentDir)).toBe(true)
-  // })
+  testViaActionsOnly('the assets/images directory exists', async () => {
+    const eaContentDir = path.join(process.cwd(), 'assets/images/early-access')
+    expect(fs.existsSync(eaContentDir)).toBe(true)
+  })
 })
