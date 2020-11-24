8c3be7d8c4d72ca52835fba9b934b02b061a6dab
Sarah Schneider <sarahs@github.com>
2020-11-24 11:49:20 -0500

    run the script to check labels at the start of the workflow; exit if none is found

--- .github/actions-scripts/enterprise-algolia-label.js
+++ .github/actions-scripts/enterprise-algolia-label.js
@@ -4,16 +4,21 @@ const fs = require('fs')
 const core = require('@actions/core')
 const eventPayload = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'))
 
-// This workflow-run script:
-// 1. Gets an array of labels on a PR
-// 2. Finds one with the relevant Algolia text
-// 3. Gets the version substring from the label string
+// This workflow-run script does the following:
+// 1. Gets an array of labels on a PR.
+// 2. Finds one with the relevant Algolia text; if none found, exits early.
+// 3. Gets the version substring from the label string.
 
 const labelText = 'sync-english-index-for-'
 const labelsArray = eventPayload.pull_request.labels
 
 // Find the relevant label
 const algoliaLabel = labelsArray.find(label => label.startsWith(labelText))
+
+// Exit early if no relevant label is found
+if (!algoliaLabel) {
+  process.exit(0)
+}
 
 // Given: sync-english-index-for-enterprise-server@3.0
 // Returns: enterprise-server@3.0
