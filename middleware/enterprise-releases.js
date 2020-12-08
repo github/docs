c52a9ba1ef28a275fcf3e1e0ef5103a82efb8839
Sarah Schneider <sarahs@github.com>
2020-12-08 10:15:23 -0500

    rename page to enterprise-server-releases

--- middleware/enterprise-releases.js
+++ middleware/enterprise-releases.js
@@ -2,7 +2,7 @@ const { liquid } = require('../lib/render-content')
 const layouts = require('../lib/layouts')
 
 module.exports = async (req, res, next) => {
-  if (!req.path.endsWith('/enterprise-releases')) return next()
+  if (!req.path.endsWith('/enterprise-server-releases')) return next()
 
-  return res.send(await liquid.parseAndRender(layouts['enterprise-releases'], req.context))
+  return res.send(await liquid.parseAndRender(layouts['enterprise-server-releases'], req.context))
 }
