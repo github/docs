a161a5923b154e0f52bac5bc068a0dd2777a909e
Sarah Schneider <sarahs@github.com>
2020-12-08 11:40:52 -0500

    lint

--- tests/helpers/conditional-runs.js
+++ tests/helpers/conditional-runs.js
@@ -1,6 +1,6 @@
-const runningActionsOnInternalRepo = process.env.GITHUB_ACTIONS === 'true' && process.env.GITHUB_REPOSITORY === 'github/docs-internal' 
+const runningActionsOnInternalRepo = process.env.GITHUB_ACTIONS === 'true' && process.env.GITHUB_REPOSITORY === 'github/docs-internal'
 
-const testViaActionsOnly = runningActionsOnInternalRepo ? test : test.skip 
+const testViaActionsOnly = runningActionsOnInternalRepo ? test : test.skip
 const describeViaActionsOnly = runningActionsOnInternalRepo ? describe : describe.skip
 
 module.exports = {
