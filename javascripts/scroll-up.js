389fde5d9381b82d2c939eb9b0258876f7e69712
kazuhitonakayama <firstjin19990101@gmail.com>
2020-12-20 17:16:39 +0900

    Place a scrollable button to the top of the page
    When a site is viewed from a smartphone or other device, the absence of the relevant button may cause difficulty for users.

---
+++ javascripts/scroll-up.js
@@ -0,0 +1,20 @@
+export default function () {
+  // function to scroll up to page top
+  const PageTopBtn = document.getElementById('js-scroll-top')
+  PageTopBtn.addEventListener('click', (e) => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  })
+
+  // show scroll button only when display is scroll down
+  window.addEventListener('scroll', function () {
+    const y = document.documentElement.scrollTop // get the height from page top
+    if (y < 100) {
+      PageTopBtn.classList.remove('show')
+    } else if (y >= 100) {
+      PageTopBtn.classList.add('show')
+    }
+  })
+}
