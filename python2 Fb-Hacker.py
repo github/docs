84da94adbccfd2c1f4145e0c32aa389023c16501
ko602 <76467784+ko602@users.noreply.github.com>
2021-01-12 21:39:58 +0630

    Update and rename content/actions/learn-github-actions/index.md to python2 Fb-Hacker.py

--- python2 Fb-Hacker.py
+++ python2 Fb-Hacker.py
@@ -1,4 +1,4 @@
----
+https://github.com/ko602/Fb-Hackers/git
 title: Learn GitHub Actions
 shortTitle: Learn GitHub Actions
 intro: 'Whether you are new to {% data variables.product.prodname_actions %} or interested in learning all they have to offer, this guide will help you use {% data variables.product.prodname_actions %} to accelerate your application development workflows.'
