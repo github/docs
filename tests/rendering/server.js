ba2f1570f6492daa425b761351e412e781898b2d
shati-patel <shati-patel@github.com>
2021-01-23 13:37:51 +0000

    change from `main`

--- tests/rendering/server.js
+++ tests/rendering/server.js
@@ -52,7 +52,8 @@ describe('server', () => {
 
     expect(csp.get('img-src').includes("'self'")).toBe(true)
     expect(csp.get('img-src').includes('github-images.s3.amazonaws.com')).toBe(true)
-
+    expect(csp.get('img-src').includes('octodex.github.com')).toBe(true)
+    
     expect(csp.get('script-src').includes("'self'")).toBe(true)
 
     expect(csp.get('style-src').includes("'self'")).toBe(true)
