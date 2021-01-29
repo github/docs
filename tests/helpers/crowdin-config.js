8d4f3e65fe7405bb923f3098b36d04a437dc8e62
Jason Etcovitch <jasonetco@github.com>
2021-01-29 10:30:51 -0500

    Move test & script utils out of `/lib` (#17517)
    * Remove an unused file
    
    * Move authenticate-to-aws to scripts/utils
    
    * Move crowdin-config to tests/utils
    
    * Remove add-frontmatter-to-file
    
    * Move find-unused-assets
    
    * Move git-utils to script/utils
    
    * Move lib/github to script/utils
    
    * Revert "Remove an unused file"
    
    This reverts commit cd93ad846a0354e957359f23124eb0724c9147cf.
    
    * Move find-extraneous-translation-files to script/utils
    
    * We already have tests/helpers
    
    * Rename script/utils => helpers for consistency
    
    * Forgot a path
    
    * Fix path to crowdin-config
    
    Co-authored-by: Chiedo John <2156688+chiedo@users.noreply.github.com>

--- tests/helpers/crowdin-config.js
+++ tests/helpers/crowdin-config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const yaml = require('js-yaml')
 
 const read = function () {
-  const filename = path.join(__dirname, '../crowdin.yml')
+  const filename = path.join(__dirname, '../../crowdin.yml')
   return yaml.load(fs.readFileSync(filename, 'utf8'), { filename })
 }
 
