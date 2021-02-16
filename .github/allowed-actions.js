6ce85ac200b7b2b92fe356589fa26471b53948df
Rachael Sewell <rachmari@github.com>
2021-02-16 13:52:46 -0800

    Update allowed-actions.js (#17873)

--- .github/allowed-actions.js
+++ .github/allowed-actions.js
@@ -24,7 +24,7 @@ module.exports = [
   'juliangruber/find-pull-request-action@2fc55e82a6d5d36fe1e7f1848f7e64fd02d99de9',
   'juliangruber/read-file-action@e0a316da496006ffd19142f0fd594a1783f3b512',
   'lee-dohm/close-matching-issues@22002609b2555fe18f52b8e2e7c07cbf5529e8a8',
-  'pascalgn/automerge-action@c9bd182',
+  'pascalgn/automerge-action@c9bd1823770819dc8fb8a5db2d11a3a95fbe9b07', //pascalgn/automerge@0.12.0
   'peter-evans/create-issue-from-file@a04ce672e3acedb1f8e416b46716ddfd09905326',
   'peter-evans/create-or-update-comment@5221bf4aa615e5c6e95bb142f9673a9c791be2cd',
   'peter-evans/create-pull-request@8c603dbb04b917a9fc2dd991dc54fef54b640b43',
