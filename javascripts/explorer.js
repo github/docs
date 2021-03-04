13d8fe66ab795ce64e826319e90edd32e52a2198
Sarah Schneider <sarahs@github.com>
2021-03-04 13:53:06 -0500

    well process.env is just not a thing in frontend javascripts

--- javascripts/explorer.js
+++ javascripts/explorer.js
@@ -1,6 +1,6 @@
-const explorerUrl = process.env.NODE_ENV === 'production'
-  ? 'https://graphql.github.com/explorer'
-  : 'http://localhost:3000'
+const explorerUrl = location.hostname === 'localhost'
+  ? 'http://localhost:3000'
+  : 'https://graphql.github.com/explorer'
 
 // Pass non-search query params to Explorer app via the iFrame
 export default function () {
