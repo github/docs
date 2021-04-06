8998e52dc0f2e71b8a3b5178747f5326b2c1128e
Mike Surowiec <mikesurowiec@users.noreply.github.com>
2021-04-06 16:28:41 -0700

    move hydro sentry error out of /events, to hydro

--- middleware/events.js
+++ middleware/events.js
@@ -2,7 +2,6 @@ const express = require('express')
 const { omit } = require('lodash')
 const Ajv = require('ajv')
 const schema = require('../lib/schema-event')
-const FailBot = require('../lib/failbot')
 
 const OMIT_FIELDS = ['type', 'token']
 
@@ -25,22 +24,7 @@ router.post('/', async function postEvents (req, res, next) {
     req.hydro.publish(
       req.hydro.schemas[fields.type],
       omit(fields, OMIT_FIELDS)
-    ).then(async (hydroRes) => {
-      // Track hydro exceptions in Sentry, but don't track 503s because we can't do anything about service availability
-      if (!hydroRes.ok && hydroRes.status !== 503) {
-        const err = new Error(`Hydro request failed: ${hydroRes.statusText}`)
-        err.status = hydroRes.status
-        err.path = fields.path
-
-        await FailBot.report(err, {
-          path: fields.path,
-          hydroStatus: hydroRes.status,
-          hydroText: hydroRes.statusText
-        })
-
-        throw err
-      }
-    }).catch((e) => {
+    ).catch((e) => {
       if (isDev) console.error(e)
     })
   }
