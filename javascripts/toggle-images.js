fc8b7152efd91ad9fc7e41b929938bb82fbc0405
Sarah Schneider <sarahs@github.com>
2021-04-16 10:12:47 -0400

    per @janiceilene: make the button outlined for the default and solid once pressed to help differentiate the two modes

--- javascripts/toggle-images.js
+++ javascripts/toggle-images.js
@@ -19,7 +19,14 @@ export default function () {
   // The button should say "Hide" by default, so we suppress the "Show" text here.
   showText.style.display = 'none'
 
+  // The selection state is set to false by default in the button HTML.
+  let selectionState = toggleImagesBtn.getAttribute('aria-selected') !== 'false'
+
   toggleImagesBtn.addEventListener('click', (e) => {
+    // On click, toggle the selection state.
+    selectionState = !selectionState
+    toggleImagesBtn.setAttribute('aria-selected', selectionState)
+
     // Check first image to see if images are currently hidden; if so, and there is a click to show them...
     if (images[0].style.display === 'none') {
       // Button should say "Hide"
