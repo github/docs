50386c9d10e2633f36497758cb0efb79ef708c8d
Sarah Schneider <sarahs@github.com>
2021-05-21 12:04:00 -0400

    use new helper module in middleware

--- middleware/contextualizers/current-product-tree.js
+++ middleware/contextualizers/current-product-tree.js
@@ -1,14 +1,15 @@
+const findPageInSiteTree = require('../../lib/find-page-in-site-tree')
+
+// This module adds currentProductTree to the context object for use in layouts.
 module.exports = function currentProductTree (req, res, next) {
   if (!req.context.page) return next()
   if (req.context.page.documentType === 'homepage') return next()
 
-  const currentSiteTree = req.context.siteTree[req.context.currentLanguage][req.context.currentVersion]
+  const currentRootTree = req.context.siteTree[req.context.currentLanguage][req.context.currentVersion]
+  const currentProductPath = `/${req.context.currentLanguage}/${req.context.currentProduct}`
+  const currentProductTree = findPageInSiteTree(currentRootTree, currentProductPath)
 
-  req.context.currentProductTree = currentSiteTree.childPages.find(page => {
-    // Find a product path that matches at least an initial part of the current path
-    const regex = new RegExp(`^${page.href}($|/)`, 'm')
-    return regex.test(req.context.currentPath)
-  })
+  req.context.currentProductTree = currentProductTree
 
   return next()
 }
