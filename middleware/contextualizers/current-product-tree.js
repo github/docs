41b11d4f9866f828977608befe582beaf992c178
Sarah Schneider <sarahs@github.com>
2021-05-21 15:10:00 -0400

    version the path

--- middleware/contextualizers/current-product-tree.js
+++ middleware/contextualizers/current-product-tree.js
@@ -1,4 +1,5 @@
 const findPageInSiteTree = require('../../lib/find-page-in-site-tree')
+const removeFPTFromPath = require('../../lib/remove-fpt-from-path')
 
 // This module adds currentProductTree to the context object for use in layouts.
 module.exports = function currentProductTree (req, res, next) {
@@ -6,7 +7,7 @@ module.exports = function currentProductTree (req, res, next) {
   if (req.context.page.documentType === 'homepage') return next()
 
   const currentRootTree = req.context.siteTree[req.context.currentLanguage][req.context.currentVersion]
-  const currentProductPath = `/${req.context.currentLanguage}/${req.context.currentProduct}`
+  const currentProductPath = removeFPTFromPath(path.posix.join(req.context.currentLanguage, req.context.currentVersion, req.context.currentProduct))
   const currentProductTree = findPageInSiteTree(currentRootTree, currentProductPath)
 
   req.context.currentProductTree = currentProductTree
