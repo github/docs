2e2e745b409c85125e27cf36a240ff431e10d844
Grace Park <gracepark@github.com>
2021-06-09 09:46:49 -0700

    update for undefined featured tracks and enum

--- components/context/ProductSubLandingContext.tsx
+++ components/context/ProductSubLandingContext.tsx
@@ -4,11 +4,11 @@ import pick from 'lodash/pick'
 export type ProductSubLandingContextT = {
   title: string,
   intro: string,
-  featuredTrack: {
+  featuredTrack?: {
     trackName: string,
     title: string,
     description: string
-    guides?: Array<{ href: string; page: { type: string }; title: string; intro: string }>;
+    guides?: Array<{ href: string; page: { type: string}; title: string; intro: string }>;
   }
 }
 
@@ -29,12 +29,14 @@ export const useProductSubLandingContext = (): ProductSubLandingContextT => {
 export const getProductSubLandingContextFromRequest = (req: any): ProductSubLandingContextT => {
   const productTree = req.context.currentProductTree
   const page = req.context.page
+  console.log(page.featuredTrack)
+  console.log(req.context.productMap[req.context.currentProduct].name)
   return {
     ...pick(page, [
       'intro',
     ]),
     title: req.context.productMap[req.context.currentProduct].name + ' guides',
     // https://github.com/vercel/next.js/issues/11993
-    featuredTrack: JSON.parse(JSON.stringify(page.featuredTrack))
+    featuredTrack: page.featuredTrack === undefined ? null : JSON.parse(JSON.stringify(page.featuredTrack))
   }
 }
--- components/landing/SubLandingHero.tsx
+++ components/landing/SubLandingHero.tsx
@@ -4,23 +4,23 @@ import { ArrowRightIcon, StarFillIcon } from '@primer/octicons-react'
 
 export const SubLandingHero = () => {
   const { title, intro, featuredTrack } = useProductSubLandingContext()
-  const guideTypes = {
-    'overview': 'Overview',
-    'quick_start': 'Quickstart',
-    'tutorial': 'Tutorial',
-    'how_to': 'How-to guide',
-    'reference': 'Reference',
+
+  enum GuideTypes {
+    overview = 'Overview',
+    quick_start = 'Quickstart',
+    tutorial = 'Tutorial',
+    how_to = 'How-to guide',
+    reference = 'Reference'
   }
 
-  console.log(featuredTrack.guides)
-  const guideItems = featuredTrack.guides?.map((guide) => 
+  const guideItems = featuredTrack?.guides?.map((guide) => 
     <li className="px-2 d-flex flex-shrink-0">
       <a href={`${guide.href}?learn=${featuredTrack.trackName}`} className="d-inline-block Box p-5 color-bg-primary color-border-primary no-underline">
         <div className="d-flex flex-justify-between flex-items-center">
           <div className="circle color-bg-primary color-text-link border-gradient--pink-blue-dark d-inline-flex">
-            <span className="m-2 f2 lh-condensed-ultra text-center text-bold step-circle-text" style={{ width: '24px', height: '24px'}}>{featuredTrack.guides?.indexOf(guide)+1}</span>
+            {featuredTrack.guides && <span className="m-2 f2 lh-condensed-ultra text-center text-bold step-circle-text" style={{ width: '24px', height: '24px'}}>{featuredTrack.guides?.indexOf(guide)+1}</span>}
           </div>
-          <div className="color-text-tertiary h6 text-uppercase">{guideTypes[guide.page.type]}</div>
+          <div className="color-text-tertiary h6 text-uppercase">{GuideTypes[guide.page.type as keyof typeof GuideTypes]}</div>
         </div>
         <h3 className="font-mktg h3-mktg my-4 color-text-primary">{guide.title}</h3>
         <div className="lead-mktg color-text-secondary f5 my-4 truncate-overflow-8">{guide.intro}</div>
@@ -41,7 +41,7 @@ export const SubLandingHero = () => {
         />
       </div>
     </header>
-    <div className="mb-6 position-relative overflow-hidden mr-n3 ml-n3 px-3">
+    {featuredTrack && <div className="mb-6 position-relative overflow-hidden mr-n3 ml-n3 px-3">
       <ul className="list-style-none d-flex flex-nowrap overflow-x-scroll px-2 feature-track">
         <li className="px-2 d-flex flex-shrink-0">
           <div className="d-inline-block Box p-5 bg-gradient--blue-pink color-text-inverse">
@@ -50,7 +50,7 @@ export const SubLandingHero = () => {
             </div>
             <h3 className="font-mktg h3-mktg my-4">{featuredTrack.title}</h3>
             <div className="lead-mktg color-text-inverse f5 my-4">{featuredTrack.description}</div>          
-            {featuredTrack && <a className="d-inline-block border color-border-inverse color-text-inverse px-4 py-2 f5 no-underline text-bold" role="button" href={`${featuredTrack.guides[0].href}?learn=${featuredTrack.trackName}`}>
+            {featuredTrack.guides && <a className="d-inline-block border color-border-inverse color-text-inverse px-4 py-2 f5 no-underline text-bold" role="button" href={`${featuredTrack.guides[0].href}?learn=${featuredTrack.trackName}`}>
               <span className="mr-2">
                 <ArrowRightIcon size={20} />
               </span>
@@ -63,6 +63,8 @@ export const SubLandingHero = () => {
       <div className="position-absolute top-0 bottom-0 left-0 ml-3 pl-3 fade-background-left"></div>
       <div className="position-absolute top-0 bottom-0 right-0 mr-3 pr-3 fade-background-right"></div>
     </div>
+    }
     </div>
+
   )
 }
