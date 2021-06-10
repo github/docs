ed4ba6dd976803c3a23bf14a84ff450acfb63249
Grace Park <gracepark@github.com>
2021-06-09 21:58:25 -0700

    get rid of sublanding section and use landing section

--- components/sublanding/ProductSubLanding.tsx
+++ components/sublanding/ProductSubLanding.tsx
@@ -1,7 +1,7 @@
 import { DefaultLayout } from 'components/DefaultLayout'
 import { useProductSubLandingContext } from 'components/context/ProductSubLandingContext'
 import React from 'react'
-import { SubLandingSection } from 'components/sublanding/SubLandingSection'
+import { LandingSection } from 'components/landing/LandingSection'
 import { SubLandingHero } from 'components/sublanding/SubLandingHero'
 import { LearningTracks } from 'components/sublanding/LearningTracks'
 import { ArticleCards } from 'components/sublanding/ArticleCards'
@@ -15,20 +15,20 @@ export const ProductSubLanding = () => {
   
   return (
     <DefaultLayout>
-      <SubLandingSection>
+      <LandingSection>
         <SubLandingHero />
-      </SubLandingSection>
+      </LandingSection>
 
       {learningTracks && learningTracks.length > 0 && (
-        <SubLandingSection title={`${title} learning paths`} className="border-top py-6" sectionLink="learning-paths" description="learning_paths_desc">
+        <LandingSection title={`${title} learning paths`} className="border-top py-6" sectionLink="learning-paths" headerClassName="mb-3" subLandingDescription="learning_paths_desc">
           <LearningTracks />
-        </SubLandingSection>
+        </LandingSection>
       )}
 
       {includeGuides && (
-        <SubLandingSection title={`All ${title} guides`} className="border-top py-6 color-border-primary" sectionLink="all-guides">
+        <LandingSection title={`All ${title} guides`} className="border-top py-6 color-border-primary" sectionLink="all-guides" headerClassName="mb-3">
           <ArticleCards />
-        </SubLandingSection>
+        </LandingSection>
       )}
     </DefaultLayout>
   )
