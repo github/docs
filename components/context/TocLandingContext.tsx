732b5fa554488c265b4674d5a14f7c99a7eff023
Mike Surowiec <mikesurowiec@users.noreply.github.com>
2021-06-10 12:23:47 -0700

    Fix Product sidebar (react) (#19843)
    * fix sidebar product rendering logic to match existing sidebar

--- components/context/TocLandingContext.tsx
+++ components/context/TocLandingContext.tsx
@@ -1,3 +1,4 @@
+import pick from 'lodash/pick'
 import { createContext, useContext } from 'react'
 
 export type TocItem = {
@@ -26,10 +27,13 @@ export const useTocLandingContext = (): TocLandingContextT => {
 }
 
 export const getTocLandingContextFromRequest = (req: any): TocLandingContextT => {
+  console.log(req.context.genericTocFlat, req.context.genericTocNested)
   return {
     title: req.context.page.title,
     introPlainText: req.context.page.introPlainText,
-    tocItems: req.context.genericTocFlat || req.context.genericTocNested || [],
+    tocItems: (req.context.genericTocFlat || req.context.genericTocNested || []).map((obj: any) =>
+      pick(obj, ['fullPath', 'title', 'intro'])
+    ),
     variant: req.context.genericTocFlat ? 'expanded' : 'compact',
   }
 }
