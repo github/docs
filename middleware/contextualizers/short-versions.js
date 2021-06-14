f3312199d8746ce8025a4ba13716d6a08dd1d1b2
Sarah Schneider <sarahs@users.noreply.github.com>
2021-06-14 15:33:18 -0400

    Update middleware/contextualizers/short-versions.js

--- middleware/contextualizers/short-versions.js
+++ middleware/contextualizers/short-versions.js
@@ -7,7 +7,6 @@
 //
 // For the custom operator handling in statements like {% if ghes > 3.0 %}, see `lib/liquid-tags/if-ver.js`.
 module.exports = async function shortVersions (req, res, next) {
-  if (!req.context.page) return next()
 
   const { allVersions, currentVersion } = req.context
   const currentVersionObj = allVersions[currentVersion]
