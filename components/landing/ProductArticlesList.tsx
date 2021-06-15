bf21cd1104d99a9f1238bfc2f7995ad09f28d475
Mike Surowiec <mikesurowiec@users.noreply.github.com>
2021-06-15 09:35:19 -0700

    fix header styling, add missing sectionLinks (#19915)

--- components/landing/ProductArticlesList.tsx
+++ components/landing/ProductArticlesList.tsx
@@ -34,7 +34,7 @@ const ArticleList = ({ treeNode }: { treeNode: ProductTreeNode }) => {
   return (
     <div className="col-12 col-lg-4 mb-6 height-full">
       <h4 className="mb-3">
-        <Link href={treeNode.href}>{treeNode.renderedFullTitle}</Link>
+        <Link className="color-unset" href={treeNode.href}>{treeNode.renderedFullTitle}</Link>
       </h4>
 
       <ul className="list-style-none">
