4fcd2b4d1c30995cc9e2337d3b0c2e592a81f34d
Anil Seervi <anil13112000@gmail.com>
2021-10-19 02:13:46 +0000

    use Link tag and relative url

--- components/page-footer/SmallFooter.tsx
+++ components/page-footer/SmallFooter.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { MarkGithubIcon } from '@primer/octicons-react'
 import { useTranslation } from 'components/hooks/useTranslation'
@@ -10,18 +11,14 @@ export const SmallFooter = () => {
       <ul className="list-style-none d-flex flex-wrap col-12 col-xl-5 flex-justify-center flex-xl-justify-between mb-2 mb-xl-0">
         <li className="mr-3 mr-xl-0">&copy; {new Date().getFullYear()} GitHub, Inc.</li>
         <li className="mr-3 mr-xl-0">
-          <a
-            href={`https://docs.github.com/${router.locale}/github/site-policy/github-terms-of-service`}
-          >
+          <Link href={`/${router.locale}/github/site-policy/github-terms-of-service`}>
             {t('terms')}
-          </a>
+          </Link>
         </li>
         <li className="mr-3 mr-xl-0">
-          <a
-            href={`https://docs.github.com/${router.locale}/github/site-policy/github-privacy-statement`}
-          >
+          <Link href={`/${router.locale}/github/site-policy/github-privacy-statement`}>
             {t('privacy')}
-          </a>
+          </Link>
         </li>
         <li className="mr-3 mr-xl-0">
           <a href="https://github.com/security">{t('product.links.security')}</a>
@@ -51,9 +48,7 @@ export const SmallFooter = () => {
           <a href="https://github.com/pricing">{t('product.links.pricing')}</a>
         </li>
         <li className="mr-3 mr-xl-0">
-          <a href={`https://docs.github.com/${router.locale}/developers`}>
-            {t('platform.links.developer_api')}
-          </a>
+          <Link href={`/${router.locale}/developers`}>{t('platform.links.developer_api')}</Link>
         </li>
         <li className="mr-3 mr-xl-0">
           <a href="https://services.github.com">{t('support.links.training')}</a>
