74a42d8f225b647b6ba26eba39ebed8710cf510c
BTCBROWSER <61902110+P7-33@users.noreply.github.com>
2021-10-30 19:30:06 -0500

    Update allowed-actions.js

--- .github/allowed-actions.js
+++ .github/allowed-actions.js
@@ -1,3 +1,4 @@
+
 // This is an AllowList of GitHub Actions that are approved for use in this project.
 // If a new or existing workflow file is updated to use an action or action version not listed here,
 // CI will fail and the action will need to be audited by the docs engineering team before it
