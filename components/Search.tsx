e1a2860adb914487c304a804a94e5d203c75a394
Mike Surowiec <mikesurowiec@users.noreply.github.com>
2022-02-09 10:23:10 -0600

    add shallow true to search to avoid triggering page data requests (#25184)

--- components/Search.tsx
+++ components/Search.tsx
@@ -132,9 +132,9 @@ export function Search({
       if (router.pathname === '/') {
         // Don't include router.locale so next doesn't attempt a
         // request to `/_next/static/chunks/pages/en.js`
-        router.replace(`/?${params.toString()}`, asPath)
+        router.replace(`/?${params.toString()}`, asPath, { shallow: true })
       } else {
-        router.replace(asPath)
+        router.replace(asPath, undefined, { shallow: true })
       }
     }
   }, [debouncedQuery])
