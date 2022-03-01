9b39fcbce37155dc98362b07ef329742ab708c32
Grace Park <gracepark@github.com>
2022-03-01 09:39:00 -0800

    Refactor parameter tables (#25676)
    * reactify rest pages
    
    * add space
    
    * speed up pages by only sending applicable rest operations
    
    * reactify rest operation part 1
    
    * remove restpage
    
    * sync translations directory with origin/main
    
    * remove translation file
    
    * clean up
    
    * fix openapi tests
    
    * fix tests
    
    * remove unused variables
    
    * add apps available page component (#25086)
    
    * simplify getRest module
    
    * add code sample component
    
    * revert translation files
    
    * revert translation files
    
    * update tests
    
    * add rest response and notes
    
    * refactor and create codeblock and add styling changes from design systems
    
    * nested all returned jsx into one single fragment
    
    * update style name
    
    * remove unnecessary div
    
    * update title and add response max height and overflow
    
    * refactor entry to RestOperation component
    
    * cleanup types and unique keys
    
    * update decorated files
    
    * Rest page data (#25346)
    
    * update endpoints for apps layout
    
    * remove null type
    
    * Update components/rest/CodeBlock.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * updating renderTableRows
    
    * cannot set p tag - should be div
    
    * Ashs new feedback on the code blocks
    
    * remove rest html pages
    
    * Removing and updating decorated files (#25484)
    
    * removing and updating decorated files
    
    * bring back default tool
    
    * fix tests
    
    * add types
    
    * fix styling in nested tables
    
    * semantic changes
    
    * adding unique key index
    
    * remove unnecessary styling
    
    * update styling for categories
    
    * place app file in own directory
    
    * update types
    
    * update function in code samples
    
    * fix some bad conflict resolution
    
    * add rest banner back to rest pages
    
    * Move all rest Markdown to data directory (#25598)
    
    * Update components/rest/RestNotes.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * adding translation (#25673)
    
    * revert change to Dockerfile
    
    * refactor parameter tables
    
    * move toc to server side
    
    * adding missing files
    
    * updating spacing
    
    * update body table
    
    * Update components/rest/BodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * adding Fragment
    
    * remove key from tr since it is on Fragment
    
    * move around imports
    
    * Update components/rest/BodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/BodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/ParameterRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/ParameterRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/BodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/BodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/BodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * small changes and adding anchor link to table instead of empty div
    
    * adding name type and description to translation
    
    * adding more translation
    
    * refactor and break out ChildBodyParameterRows
    
    * fix table css
    
    * check
    
    * change the rest parameters table to fixed to show all description
    
    * update name to match
    
    * run prettier
    
    * Update components/rest/RestCodeSamples.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/ChildBodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/ChildBodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * Update components/rest/BodyParametersRows.tsx
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    
    * updating CHildBodyParameterRows
    
    * run prettier
    
    Co-authored-by: Rachael Sewell <rachmari@github.com>
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>

--- components/rest/CodeBlock.tsx
+++ components/rest/CodeBlock.tsx
@@ -15,10 +15,10 @@ export function CodeBlock({ verb, codeBlock, setHTML = false }: Props) {
       dangerouslySetInnerHTML={{ __html: codeBlock }}
     />
   ) : (
-    <pre className={cx(styles.methodCodeBlock, 'mb-3 rounded-1 border')}>
+    <pre className={cx(styles.methodCodeBlock, 'rounded-1 border')}>
       <code>
         {verb && (
-          <span className="color-bg-accent-emphasis color-fg-on-emphasis rounded-1 px-2 py-1 text-uppercase">
+          <span className="color-bg-accent-emphasis color-fg-on-emphasis rounded-1 text-uppercase">
             {verb}
           </span>
         )}{' '}
