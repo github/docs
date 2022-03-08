1a6bee46014b5ca29ee6673464883108a119cd12
Grace Park <gracepark@github.com>
2022-03-08 10:00:38 -0800

    Refactor to add RestReferencePage (#25781)
    * refactor to add RestReferencePage
    
    * remove structured data from article page
    
    * add back clientsidehightlight js and remove structuredcontent
    
    * update endpoint available for github apps page to use RestReferencePage
    
    * check for minitocs
    
    * use children for ArticlePage for overview endpoints page and move category data to RestReferencePage
    
    * create MiniTocs component
    
    * add MiniTocs
    
    * fix unique key issue
    
    * break into out into own div
    
    * remove some unnecessary ts
    
    * refactor looping section
    
    * simplify markdown component usage
    
    * add key prop
    
    * add key prop
    
    * add unique key
    
    Co-authored-by: Rachael Sewell <rachmari@github.com>

--- components/article/ArticlePage.tsx
+++ components/article/ArticlePage.tsx
@@ -1,8 +1,6 @@
-import { useState, useEffect, ReactNode } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import cx from 'classnames'
-import { ActionList, Heading } from '@primer/react'
 
 import { ZapIcon, InfoIcon, ShieldLockIcon } from '@primer/octicons-react'
 import { Callout } from 'components/ui/Callout'
@@ -10,7 +8,7 @@ import { Callout } from 'components/ui/Callout'
 import { Link } from 'components/Link'
 import { DefaultLayout } from 'components/DefaultLayout'
 import { ArticleTitle } from 'components/article/ArticleTitle'
-import { MiniTocItem, useArticleContext } from 'components/context/ArticleContext'
+import { useArticleContext } from 'components/context/ArticleContext'
 import { useTranslation } from 'components/hooks/useTranslation'
 import { LearningTrackNav } from './LearningTrackNav'
 import { MarkdownContent } from 'components/ui/MarkdownContent'
@@ -18,10 +16,8 @@ import { Lead } from 'components/ui/Lead'
 import { ArticleGridLayout } from './ArticleGridLayout'
 import { PlatformPicker } from 'components/article/PlatformPicker'
 import { ToolPicker } from 'components/article/ToolPicker'
+import { MiniTocs } from 'components/ui/MiniTocs'
 
-const ClientSideRedirectExceptions = dynamic(() => import('./ClientsideRedirectExceptions'), {
-  ssr: false,
-})
 const ClientSideHighlightJS = dynamic(() => import('./ClientSideHighlightJS'), { ssr: false })
 
 // Mapping of a "normal" article to it's interactive counterpart
@@ -49,12 +45,11 @@ const interactiveAlternatives: Record<string, { href: string }> = {
       href: '/codespaces/setting-up-your-project-for-codespaces/setting-up-your-project-for-codespaces?langId=py',
     },
 }
-
-export type StructuredContentT = {
-  structuredContent?: ReactNode
+type Props = {
+  children?: React.ReactNode
 }
 
-export const ArticlePage = ({ structuredContent }: StructuredContentT) => {
+export const ArticlePage = ({ children }: Props) => {
   const { asPath } = useRouter()
   const {
     title,
@@ -69,57 +64,8 @@ export const ArticlePage = ({ structuredContent }: StructuredContentT) => {
     miniTocItems,
     currentLearningTrack,
   } = useArticleContext()
-  const renderedContent = structuredContent || renderedPage
   const { t } = useTranslation('pages')
   const currentPath = asPath.split('?')[0]
-
-  const renderTocItem = (item: MiniTocItem) => {
-    return (
-      <ActionList.Item
-        as="li"
-        key={item.contents}
-        className={item.platform}
-        sx={{ listStyle: 'none', padding: '2px' }}
-      >
-        <div className={cx('lh-condensed d-block width-full')}>
-          <div dangerouslySetInnerHTML={{ __html: item.contents }} />
-          {item.items && item.items.length > 0 ? (
-            <ul className="ml-3">{item.items.map(renderTocItem)}</ul>
-          ) : null}
-        </div>
-      </ActionList.Item>
-    )
-  }
-
-  // We have some one-off redirects for rest api docs
-  // currently those are limited to the repos page, but
-  // that will grow soon as we restructure the rest api docs.
-  // This is a workaround to updating the hardcoded links
-  // directly in the REST API code in a separate repo, which
-  // requires many file changes and teams to sign off.
-  // While the organization is turbulent, we can do this.
-  // Once it's more settled, we can refactor the rest api code
-  // to leverage the OpenAPI urls rather than hardcoded urls.
-  // The code below determines if we should bother loading this redirecting
-  // component at all.
-  // The reason this isn't done at the server-level is because there you
-  // can't possibly access the URL hash. That's only known in client-side
-  // code.
-  const [loadClientsideRedirectExceptions, setLoadClientsideRedirectExceptions] = useState(false)
-  useEffect(() => {
-    const { hash, pathname } = window.location
-    // Today, Jan 2022, it's known explicitly what the pathname.
-    // In the future there might be more.
-    // Hopefully, we can some day delete all of this and no longer
-    // be dependent on the URL hash to do the redirect.
-    if (
-      hash &&
-      (pathname.endsWith('/rest/reference/repos') ||
-        pathname.endsWith('/rest/reference/enterprise-admin'))
-    ) {
-      setLoadClientsideRedirectExceptions(true)
-    }
-  }, [])
 
   // If the page contains `[data-highlight]` blocks, these pages need
   // syntax highlighting. But not every page needs it, so it's conditionally
@@ -139,29 +85,8 @@ export const ArticlePage = ({ structuredContent }: StructuredContentT) => {
     // consecutive one does.
   }, [asPath])
 
-  // Scrollable code blocks in our REST API docs and elsewhere aren't accessible
-  // via keyboard navigation without setting tabindex="0".  But we don't want to set
-  // this attribute on every `<pre>` code block, only the ones where there are scroll
-  // bars because the content isn't all visible.
-  useEffect(() => {
-    const codeBlocks = document.querySelectorAll<HTMLPreElement>('pre')
-
-    codeBlocks.forEach((codeBlock) => {
-      if (
-        codeBlock.scrollWidth > codeBlock.clientWidth ||
-        codeBlock.scrollHeight > codeBlock.clientHeight
-      ) {
-        codeBlock.setAttribute('tabindex', '0')
-      }
-    })
-  }, [])
-
   return (
     <DefaultLayout>
-      {/* Doesn't matter *where* this is included because it will
-      never render anything. It always just return null. */}
-      {loadClientsideRedirectExceptions && <ClientSideRedirectExceptions />}
-
       {/* Doesn't matter *where* this is included because it will
       never render anything. It always just return null. */}
       {lazyLoadHighlightJS && <ClientSideHighlightJS />}
@@ -220,28 +145,13 @@ export const ArticlePage = ({ structuredContent }: StructuredContentT) => {
                 </div>
               )}
               {miniTocItems.length > 1 && (
-                <>
-                  <Heading as="h2" id="in-this-article" className="mb-1" sx={{ fontSize: 1 }}>
-                    <Link href="#in-this-article">{t('miniToc')}</Link>
-                  </Heading>
-
-                  <ActionList
-                    key={title}
-                    items={miniTocItems.map((items, i) => {
-                      return {
-                        key: title + i,
-                        text: title,
-                        renderItem: () => <ul>{renderTocItem(items)}</ul>,
-                      }
-                    })}
-                  />
-                </>
+                <MiniTocs pageTitle={title} miniTocItems={miniTocItems} />
               )}
             </>
           }
         >
           <div id="article-contents">
-            <MarkdownContent>{renderedContent}</MarkdownContent>
+            <MarkdownContent>{children || renderedPage}</MarkdownContent>
             {effectiveDate && (
               <div className="mt-4" id="effectiveDate">
                 Effective as of:{' '}
