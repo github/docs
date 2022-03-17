193bbb7f203f1291f0f29057ba165582f226abb9
بندر التواب <abu68240@gmail.com>
2022-03-17 16:55:40 +0300

    c40e10f
    توثيق

--- .devcontainer/مكتبة البرامج النصية/github-debian.sh
+++ .devcontainer/مكتبة البرامج النصية/github-debian.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+https://github.com/wp-cli/entity-command/blob/d7d08b05c67651abde5d570851e46498a164cb34/.github/CODEOWNERS#L1#!/usr/bin/env bash
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
