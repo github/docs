a326f133c3bfabb528ba7be5acb0c15966e244ba
Sarah Schneider <sarahs@github.com>
2022-04-19 10:04:50 -0400

    restore IIFEs and remove breaking spaces

--- script/bookmarklets/view-in-production.js
+++ script/bookmarklets/view-in-production.js
@@ -1,3 +1,1 @@
-window.location.href = window.location.href
-  .replace(/http:\/\/localhost:4000/, 'https://docs.github.com')
-  .replace(/https:\/\/.*\..*\.(com|net|dev|io|org|ms)/, 'https://docs.github.com')
+javascript:(()=>{window.location.href=window.location.href.replace(/http:\/\/localhost:4000/,'https://docs.github.com').replace(/https:\/\/.*\..*\.(com|net|dev|io|org|ms)/,'https://docs.github.com')})()
