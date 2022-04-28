b2db816d662c467e7babf5fae0216642c5aac5d4
Sarah Schneider <sarahs@github.com>
2022-04-28 16:27:07 -0400

    update test

--- tests/routing/versionless-redirects.js
+++ tests/routing/versionless-redirects.js
@@ -2,6 +2,7 @@ import { jest } from '@jest/globals'
 
 import { get } from '../helpers/e2etest.js'
 import getExceptionRedirects from '../helpers/exception-redirects.js'
+import { latest } from '../../lib/enterprise-server-releases.js'
 
 describe('versioned redirects', () => {
   jest.setTimeout(5 * 60 * 1000)
@@ -10,7 +11,10 @@ describe('versioned redirects', () => {
 
   test.each(Object.keys(exceptionRedirects))('responds with 200 on %p', async (oldPath) => {
     const newPath = exceptionRedirects[oldPath]
-    const englishNewPath = `/en${newPath}`
+    const englishNewPath = `/en${newPath.replace(
+      '/enterprise-server@latest',
+      `/enterprise-server@${latest}`
+    )}`
     const { statusCode, headers } = await get(oldPath, { followRedirects: false })
     expect(statusCode).toBe(302)
     expect(headers.location).toBe(englishNewPath)
