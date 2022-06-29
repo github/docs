ee1f20f4fb41593e6ea08a318bf0001fe1c36a2c
Peter Bengtsson <peterbe@github.com>
2022-06-29 13:55:03 +0200

    jest 28.1.1 (#28797)
    * got 28.1.1
    
    * wip
    
    * force upgrade nth-check
    
    * legacyFakeTimers

--- tests/routing/release-notes.js
+++ tests/routing/release-notes.js
@@ -4,7 +4,7 @@ import nock from 'nock'
 import { get, getDOM } from '../helpers/e2etest.js'
 import enterpriseServerReleases from '../../lib/enterprise-server-releases.js'
 
-jest.useFakeTimers('legacy')
+jest.useFakeTimers({ legacyFakeTimers: true })
 
 describe('release notes', () => {
   jest.setTimeout(60 * 1000)
