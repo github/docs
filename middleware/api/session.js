0886faddac5e96122a95c35f14be545eb6f449fd
Kevin Heis <heiskr@users.noreply.github.com>
2022-07-28 11:50:46 -0700

    Move language context to browser/session (#29433)

--- middleware/api/session.js
+++ middleware/api/session.js
@@ -10,8 +10,8 @@ router.get('/', (req, res) => {
   res.json({
     isSignedIn: Boolean(req.cookies?.dotcom_user),
     csrfToken: req.csrfToken?.() || '',
-    language: req.language,
     userLanguage: req.userLanguage,
+    languages: req.context.languages,
     theme: getTheme(req),
     themeCss: getTheme(req, true),
   })
