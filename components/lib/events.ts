9d0cb690a71eaf69dcfc465eb5848bc7f669307c
Kevin Heis <heiskr@users.noreply.github.com>
2022-07-28 12:51:32 -0700

    Let  be the caller, so events doesnt 403 on the first call (#29441)
    * Let  be the caller, so events doesnt 403 on the first call
    
    * Declare function

--- components/lib/events.ts
+++ components/lib/events.ts
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from 'uuid'
 import Cookies from 'js-cookie'
 import parseUserAgent from './user-agent'
-import { getSession, fetchSession } from './get-session'
+import { getSession } from './get-session'
 
 const COOKIE_NAME = '_docs-events'
 
@@ -274,8 +274,13 @@ function initPrintEvent() {
   })
 }
 
+function waitForSession(fn: Function) {
+  if (getSession()) return fn()
+  setTimeout(() => waitForSession(fn), 300)
+}
+
 export default function initializeEvents() {
-  fetchSession().then(() => {
+  waitForSession(() => {
     initPageAndExitEvent() // must come first
     initLinkEvent()
     initClipboardEvent()
