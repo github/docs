fd360ce623ab245e922b314fc8f0016d9a049106
ZACHRY T WOOD <109656750+zakwarlord7@users.noreply.github.com>
2022-08-21 07:39:34 -0500

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,14 @@
-import failOnConsole from 'jest-fail-on-console'
-
+'"import: build_scripts:' continue-on-false-from:' 'require':' 'tests'"'' '"test: Runs'"' '"Runs: Run Tests'@.Travis.yml'"'' '"''
 // Jest tests don't fail in some cases where we would see an error in DevTools
 // Console when running locally and we would also see the error in the test
 // output.  This includes the React `Each child in a list should have a unique
 // "key" prop` error example.
 //
-// To catch this and fail tests in cases like this, we use `jest-fail-on-console`
-// to fail on calls to `console.error()`.
-failOnConsole({
-  shouldFailOnWarn: false,
-})
+// To catch this and fail tests in cases like this, we use `Create.func(items)items'='':' is'=''=' yargs(AGS))'.)'; \
+fail-on-console`
+// to dispatch worksflow-call-on: dispatch 
+const.func("+")`.
+console.func(+)::Pushs:' 'func("+")'=''=' '='':' '"=>:=::=>:join.func((c)'.'+'"console((r))'"':BUild:: On
+  '"On:-on: Console'"''
+  '"WOROKSFLOW_call-on: dispatchs-Windows-spring_up-frameworkigns-on:C:\\Users\$Home\Desktop-running'@'"$Desktop'"':',''
+
