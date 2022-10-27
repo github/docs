64ee6b2f0916324c2db3779558362c35512de6f4
Peter Bengtsson <peterbe@github.com>
2022-10-27 22:13:00 +0200

    optimize the getCurrentProductTree function (#31620)

--- middleware/contextualizers/current-product-tree.js
+++ middleware/contextualizers/current-product-tree.js
@@ -1,9 +1,10 @@
 import path from 'path'
+import liquid from '../../lib/render-content/liquid.js'
 import findPageInSiteTree from '../../lib/find-page-in-site-tree.js'
 import removeFPTFromPath from '../../lib/remove-fpt-from-path.js'
 
 // This module adds currentProductTree to the context object for use in layouts.
-export default function currentProductTree(req, res, next) {
+export default async function currentProductTree(req, res, next) {
   if (!req.context.page) return next()
   if (req.context.page.documentType === 'homepage') return next()
 
@@ -20,13 +21,68 @@ export default function currentProductTree(req, res, next) {
       req.context.currentProduct
     )
   )
-  const currentProductTree = findPageInSiteTree(
+  req.context.currentProductTree = findPageInSiteTree(
     currentRootTree,
     req.context.currentEnglishTree,
     currentProductPath
   )
 
-  req.context.currentProductTree = currentProductTree
+  // First make a slim tree of just the 'href', 'title', 'shortTitle'
+  // 'documentType' and 'childPages' (which is recursive).
+  // This gets used for map topic and category pages.
+  req.context.currentProductTreeTitles = await getCurrentProductTreeTitles(
+    req.context.currentProductTree,
+    req.context
+  )
+  // Now make an even slimmer version that excludes all hidden pages.
+  // This is i used for sidebars.
+  req.context.currentProductTreeTitlesExcludeHidden = excludeHidden(
+    req.context.currentProductTreeTitles
+  )
 
   return next()
+}
+
+// Return a nested object that contains the bits and pieces we need
+// for the tree which is used for sidebars and listing
+async function getCurrentProductTreeTitles(input, context) {
+  const { page } = input
+  const childPages = await Promise.all(
+    (input.childPages || []).map((child) => getCurrentProductTreeTitles(child, context))
+  )
+
+  const renderedFullTitle = page.rawTitle.includes('{')
+    ? await liquid.parseAndRender(page.rawTitle, context)
+    : page.rawTitle
+  let renderedShortTitle = ''
+  if (page.rawShortTitle) {
+    renderedShortTitle = page.rawShortTitle.includes('{')
+      ? await liquid.parseAndRender(page.rawShortTitle, context)
+      : page.rawShortTitle
+  }
+
+  const node = {
+    href: input.href,
+    title: renderedFullTitle,
+    shortTitle:
+      renderedShortTitle && (renderedShortTitle || '') !== renderedFullTitle
+        ? renderedShortTitle
+        : '',
+    documentType: page.documentType,
+    childPages: childPages.filter(Boolean),
+  }
+  if (page.hidden) node.hidden = true
+  return node
+}
+
+function excludeHidden(tree) {
+  if (tree.hidden) return null
+  const newTree = {
+    href: tree.href,
+    title: tree.title,
+    shortTitle: tree.shortTitle,
+    documentType: tree.documentType,
+    childPages: tree.childPages.map(excludeHidden).filter(Boolean),
+  }
+  return newTree
 }
