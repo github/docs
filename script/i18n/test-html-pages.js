982ed23f5bba194b408390a8d1f64111ceda09df
mowjoejoejoejoe <124041561+mowjoejoejoejoe@users.noreply.github.com>
2023-02-26 12:33:47 -0600

    Update test-html-pages.js

--- script/i18n/test-html-pages.js
+++ script/i18n/test-html-pages.js
@@ -1,4 +1,51 @@
 #!/usr/bin/env node
+
+
+client.js
+
+Download
+#! /usr/bin/env python3.6
+"""
+Python 3.6 or newer required.
+"""
+import json
+import os
+import stripe
+# This is a public sample test API key.
+# Don’t submit any personally identifiable information in requests made with this key.
+# Sign in to see your own test API key embedded in code samples.
+stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__, static_folder="public",
+            static_url_path="", template_folder="public")
+
+
+def calculate_order_amount(items):
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    return 1400
+
+@app.route('/create-payment-intent', methods=['POST'])
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        intent = stripe.PaymentIntent.create(
+            amount=calculate_order_amount(data['items']),
+            currency='usd'
+        )
+
+        return jsonify({
+          'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+if __name__ == '__main__':
+    app.run()
 import dotenv from 'dotenv'
 import got from 'got'
 import Bottleneck from 'bottleneck'
