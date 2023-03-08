7232daa5e1b73bb54c3d066be24fa186139685d8
Rachael Sewell <rachmari@github.com>
2023-03-08 09:50:23 -0800

    add sha of sync pipeline data to config file (#35335)
    Co-authored-by: Peter Bengtsson <peterbe@github.com>

--- src/rest/scripts/utils/get-openapi-schemas.js
+++ src/rest/scripts/utils/get-openapi-schemas.js
@@ -5,7 +5,7 @@ import path from 'path'
 import { allVersions } from '../../../../lib/all-versions.js'
 
 const OPEN_API_RELEASES_DIR = '../github/app/api/description/config/releases'
-const metaData = JSON.parse(await readFile('src/rest/data/meta.json', 'utf8'))
+const configData = JSON.parse(await readFile('src/rest/lib/config.json', 'utf8'))
 // Gets the full list of unpublished + active, deprecated + active,
 // or active schemas from the github/github repo
 // `openApiReleaseDir` is the path to the `app/api/description/config/releases`
@@ -25,19 +25,19 @@ export async function getSchemas(directory = OPEN_API_RELEASES_DIR) {
     const content = await readFile(path.join(directory, file), 'utf8')
     const yamlContent = yaml.load(content)
 
-    const releaseMatch = Object.keys(metaData.versionMapping).find((name) =>
+    const releaseMatch = Object.keys(configData.versionMapping).find((name) =>
       fileBaseName.startsWith(name)
     )
     if (!releaseMatch) {
       throw new Error(
         `🛑 The file ${fileBaseName} does not match any known docs version name. (not one of ${Object.keys(
-          metaData.versionMapping
+          configData.versionMapping
         )})`
       )
     }
     const docsName =
-      metaData.versionMapping[fileBaseName] ||
-      fileBaseName.replace(releaseMatch, metaData.versionMapping[releaseMatch])
+      configData.versionMapping[fileBaseName] ||
+      fileBaseName.replace(releaseMatch, configData.versionMapping[releaseMatch])
     const isDeprecatedInDocs = !Object.keys(allVersions).find(
       (version) => allVersions[version].openApiVersionName === docsName
     )
