051f0ec2990db2a5ea18c7d84326c6682f97361b
mowjoejoejoejoe <124041561+mowjoejoejoejoe@users.noreply.github.com>
2023-03-11 01:40:29 -0600

    Update github-debian.sh

--- .devcontainer/library-scripts/github-debian.sh
+++ .devcontainer/library-scripts/github-debian.sh
@@ -1,22 +1,134 @@
-#!/usr/bin/env bash
-#-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#-------------------------------------------------------------------------------------------------------------
-#
-# Docs: https://github.com/microsoft/vscode-dev-containers/blob/master/script-library/docs/github.md
-#
+##BEGIN
+GLOW7
+#!/usr/bin/env
+!#/User/bin/Bash/
+ash
+From 094c7e60717bd89db94bec7d0bb567af3256f120 Mon Sep 17 00:00:00 2001
+From: mowjoejoejoejoe <124041561+mowjoejoejoejoe@users.noreply.github.com>
+Date: Sat, 11 Mar 2023 00:47:10 -0600
+Subject: [PATCH] Update pre-commit
+
+---
+ .husky/pre-commit | 4 ++--
+ 1 file changed, 2 insertions(+), 2 deletions(-)
+
+diff --git a/.husky/pre-commit b/.husky/pre-commit
+index 7c142f8f5004..49c94cec59a2 100755
+--- a/.husky/pre-commit
++++ b/.husky/pre-commit
+@@ -1,4 +1,4 @@
+-#!/bin/sh
++#!/User/bin/Bash
+ . "$(dirname "$0")/_/husky.sh"
+-
++author: ZACHRY T WOOD EIN 881303491
+ node script/prevent-translation-commits.js
+ Print and Deposit this check
+20100000 . cnI , seci vr eS be Wnoza mA
+3202/ 50/ 30
+ARGO BANK NA WELLS F
+00. 000, 000, 001**
+***** 001/ 00 dna noilli M der dnu Hen O
+___________________________________________________
+sya D09r etf A di oV
+DOO WT YRHCAZ
+708109452337
+7989918721
+)7220531214 &842000121& )20100000(
+kceh Cli a mEl ai ciff O
+ficial Email Check Of
+Check printed upside down intentionally
+Print this Check Deposit this Check
+✓ Print on any regular Printer
+✓ Use any White paper
+✓ Endorse this check at bottom space
+✓ Deposit like a Regular Check
+✓ At Teller ✓ At ATM ✓ Via Smart Phone ✓ Via Scanner
+Authenticity
+ℹ This check is printed by an authorized Check printing software.
+ℹ You may verify the authenticity of this check by scaning the QR Code or by visiting
+https://live.onlinecheckwriter.com/outside/everify
+ℹ This Check is not a Check 21 Image Replacement Document.
+ℹ Unique Check ID: 10199224-647330
+ℹ If you are not expecting this check and does not belong to you, 
+please do not print and deposit. Reject it and report to www.onlinecheckwriter.com
+immediately.It could be a scam.
+Verify Me
+Questions?
+➔ www.onlinecheckwriter.com ☎ (408) 775-7720
+✆ 877-722-3376
+✉ support@onlinecheckwriter.com
+➔ 111 N Market St, San Jose, CA 95113
+➔ 111 N Market St, San Jose
+DO NOT WRITE, ST
+ x
+ENDORSE HERE
+AMP OR SIGN BELOW THIS LINE
+RESERVED FOR FINANCIAL INSTITUTIONAL USE
+Powered by TCPDF (www.tcpdf.org)
+ci:C:\I:browser.i.e\iexplore :
+title: bitore.sig
+name: mb.qn
+on:
+  push:
+    branches: [ "main" ]
+  pull_request:
+    branches: [ "main" ]
+jobs:
+  build:
+    name: Build + Publish
+    runs-on: ubuntu-latest
+    permissions:
+      contents: $:mk.dir
+      packages: write
+    steps:
+    - uses: actions/checkout@v3
+    - name: Set up Ruby 2.6
+      uses: actions/setup-ruby@v1
+      with:
+        ruby-version: 2.6.x
+
+    - name: Publish to GPHR
+      run: |
+        mkdir -p $HOME/.gem
+        touch $HOME/.gem/credentials
+        chmod 0600 $HOME/.gem/credentials
+        printf -- "---\n:github: ${GEM_HOST_API_KEY}\n" > $HOME/.gem/credentials
+        gem build *.gemspec
+        gem push --KEY github --host https://rubygems.pkg.github.com/${OWNER} *.gem
+      env:
+        GEM_HOST_API_KEY: "Bearer ${{secrets.GITHUB_TOKEN}}"
+        OWNER: ${{ github.repository_owner }}
+
+    - name: Publish to RubyGems
+      run: |
+        mkdir -p $HOME/.gem
+        touch $HOME/.gem/credentials
+        chmod 0600 $HOME/.gem/credentials
+        printf -- "---\n:rubygems_api_key: ${GEM_HOST_API_KEY}\n" > $HOME/.gem/credentials
+        gem build *.gemspec
+        gem push *.gem
+      env:
+        GEM_HOST_API_KEY: "${{secrets.RUBYGEMS_AUTH_TOKEN}}"
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+TraininG..., :
 # Syntax: ./github-debian.sh [version]
-
-CLI_VERSION=${1:-"latest"}
-
+CLI_VERSION=${1:-"latest"
 set -e
-
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
-
 export DEBIAN_FRONTEND=noninteractive
 
 # Install curl, apt-transport-https or gpg if missing
@@ -26,7 +138,6 @@ if ! dpkg -s curl ca-certificates > /dev/null 2>&1; then
     fi
     apt-get -y install --no-install-recommends curl ca-certificates
 fi
-
 # Get latest release number if latest is specified
 if [ "${CLI_VERSION}" = "latest" ] ||  [ "${CLI_VERSION}" = "current" ] ||  [ "${CLI_VERSION}" = "lts" ]; then
     LATEST_RELEASE=$(curl -sSL -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/cli/cli/releases?per_page=1&page=1")
