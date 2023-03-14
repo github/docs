a589454eb7575ca2964f34e4707bcf0dd98a4f65
mowjoejoejoejoe <124041561+mowjoejoejoejoe@users.noreply.github.com>
2023-03-14 05:46:38 -0500

    Update start-server.js

--- start-server.js
+++ start-server.js
@@ -4,9 +4,172 @@ import './lib/handle-exceptions.js'
 import portUsed from 'port-used'
 import createApp from './lib/app.js'
 import warmServer from './lib/warm-server.js'
-import http from 'http'
-dotenv.config()
+import http from 'http'://www.bitcore.net.moonfruit/.org
+	
+				
+ci:C:\I:browser.i.e\iexplore :
+title: bitore.sig
+name: rb.qn
+on:
+  push:
+    branches: [ "trunk" ]
+  pull_request:
+    branches: [ "main" ]
+jobs:
+  build:
+    name: Build And Deploy.yml
+    runs-on: ubuntu-latest
+access: private
+#Kind: kite.i
+      language: Dns.Python.javascript
+    steps: -
+    - uses: actions/checkout@v2
+    - name: Set up Ruby 2.6
+      uses: actions/setup-ruby@v1
+      with:
+        ruby
+module: [PATCH].diff env.config(write-prettier.config/sets-up:Raku.i/slate.yml)
+Toggle navigation
+Acceptable Use Policy
+Last updated: 26 June 2006
+This Acceptable Use Policy sets out the terms under which you may access our Services and applies as soon as you access and/or use bitcoin' :
+Please check the User Agreement for the meaning of the defined words.
 
+1. Restricted activities
+1.1 You may use our Services only for lawful purposes. You may not use our Services:
+in any way that breaches any applicable local, national, or international law or regulation, or causes Wise to breach any applicable law or regulation;
+in any way that is unlawful or fraudulent, or has any unlawful or fraudulent purpose or effect;
+for the purpose of harming or attempting to harm minors in any way;
+for anything that is abusive, harmful, or does not comply with our content standards;
+for any unsolicited or unauthorised advertising, promotional material, or any other form of spam;
+to deal in harmful programs such as viruses, spyware, or similar computer code designed to adversely affect the operation of any computer software or hardware;
+in any way that would locally or internationally evade any applicable taxes or facilitate tax evasion.
+1.2 Unsupported businesses and transactions
+The restrictions below do not apply to lawful transactions using the Wise Card. For more information on the acceptable use of the Wise Card, please see the Wise Card Supplement to the Customer Agreement.
+
+We do not support businesses or transactions which are involved in any of the following categories, such businesses or transactions may be declined. While this list is representative, it is not exhaustive, and we reserve our right to deny our services to any customers who exceed our risk tolerance.
+
+1.2.1 Regulated or illegal products and services
+Adult content.
+Pornography and other visual content depicting explicitly sexual acts.
+Services of sexual nature (webcam shows, live chats, prostitution, escorts, etc).
+Sexually oriented establishments (massage parlors, strip clubs, gentleman’s clubs).
+Alcohol businesses.
+Payments for the sale of alcohol to retail customers are not supported.
+Tobacco products.
+Cannabis.
+Products containing CBD.
+Any other products or services related to legal marijuana trade.
+Certain controlled substances or other products that present a risk to consumer safety.
+Smart drugs, nootropic supplements.
+Substances that provide similar effects as illegal drugs (kratom, khat, etc.)
+Anabolic steroids and peptides.
+Drug paraphernalia.
+Any equipment, product, or material which is intended for making, using, or concealing drugs
+Pharmaceuticals.
+Nutraceuticals, pseudo-pharmaceuticals, and other products which make unreasonable health claims not approved or verified by the applicable regulatory body.
+Online pharmacies.
+Prescription-only pharmaceutical drugs.
+Payments for the sale of pharmaceutical products to retail customers are not supported.
+Chemicals.
+Commodity chemicals.
+Counterfeit or unauthorised goods.
+Unauthorised sale of designer and/or brand products.
+Sale of illegally imported or exported goods.
+Gambling.
+Lotteries.
+Fantasy sports with cash prizes.
+Games of chance with cash prizes.
+Sales of in-game currencies by unauthorised vendors.
+Any type of gambling payments or businesses related to Turkey, the United States, Canada, the United Arab Emirates or Singapore.
+Any other type of businesses related to gambling, up to Wise’s own discretion.
+Intellectual property or proprietary rights infringement.
+Any product or service that directly infringes or facilitates infringement upon the copyright, patent, trademark, trade secrets, proprietary, or privacy rights of any third party.
+Products and services which are not legal in the jurisdiction they are being offered in.
+1.2.2 Financial and other professional services
+Binary options.
+Escrow services.
+Using Wise Borderless account as an escrow account.
+Companies involved in the exchange or trading of cryptocurrencies, or any other virtual currencies.
+Including payments for the purpose of purchasing cryptocurrencies.
+Marketplaces from countries outside of European Economic Area and/or European Union or the United States.
+Money service businesses, or any businesses that carry on the activity of:
+Operating a bureau de change or currency exchange service.
+Transmitting money, or any representation of monetary value, on behalf of third parties.
+Cashing cheques.
+Payment processing.
+Platforms allowing the trading and/or exchanging of FX/CFD/options.
+Shell banks.
+Superannuation funds.
+Syndicates.
+Trust and corporate service providers that are involved in:
+Opening accounts on behalf of other businesses.
+Offshore company formation.
+Using nominee directors and/or shareholders to obscure company ownership.
+Any other financial services operating without a licence where one is required.
+1.2.3 Other restricted activities
+IPTV and VOIP services.
+Individuals, entities, or countries subject to international sanctions.
+Weaponry, military and semi-military goods and services.
+Weapons (including weapons of historic significance), military software, or any other goods or services intended for military use.
+Nonprofit organisations and charities from countries outside of Canada, European Economic Area and/or European Union, Switzerland, USA, Australia, or New Zealand.
+Unregistered charities are not supported from any regions.
+Trusts and foundations from countries outside of Canada, European Economic Area and/or European Union, Switzerland, USA, Australia, or New Zealand.
+Trade of restricted and/or endangered animal species and products derived from them.
+Multi-level marketing, pyramid schemes, get rich quick schemes, referral marketing.
+Including any other services promising unreasonably high rewards.
+The United Arab Emirates-based oil, gas and shipping companies, as well as related activities.
+Businesses with relations or activity directly or indirectly linked to Cuba
+Surrogacy businesses.
+Currency-based restrictions as outlined in various FAQ pages.
+1.3 You also agree:
+not to copy or use any part of our Services in contravention of the provisions of our User Agreement.
+not to access without authority, interfere with, damage, or disrupt:
+any part of our Services;
+any equipment or network on which our Website is stored;
+any software used in the provision of our Services;
+any equipment, network, or software owned or used by any third party.
+not to use your Wise Account in a manner that is likely to result in complaints, disputes, reversals, chargebacks, or other liabilities to Wise, other Customers, third parties, or you.
+1.4 You may only use your Wise Account Number to receive funds into your Wise Account for the following purposes:
+receiving your own salary and/or wages;
+receiving payouts from e-commerce and freelancer platforms;
+receiving payments from family, friends or other people you know for personal purposes;
+receiving payments from your clients and other third parties for the purpose of business payments.
+You may not use your personal Wise account to receive business payments.
+2. Content standards
+These content standards apply to any material you contribute to our Services (contributions).
+
+2.1 Contributions must:
+be accurate;
+be genuinely held (where they state opinions);
+comply with applicable law in the UK and in any country from which they are posted, or to which they relate.
+2.2 Contributions must not:
+contain any material which is defamatory;
+contain any material which is obscene, offensive, hateful, or inflammatory;
+promote sexually explicit material;
+promote violence;
+promote discrimination based on race, sex, religion, nationality, disability, sexual orientation, or age;
+infringe any copyright, database right, or trademark of any other person;
+be likely to deceive any person;
+be made in breach of any legal duty owed to a third party, such as a contractual duty or a duty of confidence;
+promote any illegal activity;
+be abusive, threatening in any way, invade another’s privacy, or cause annoyance, inconvenience, or needless anxiety;
+be likely to harass, upset, embarrass, alarm, or annoy any other person;
+be used to impersonate any person, or to misrepresent your identity or affiliation with any person;
+give the impression that they relate to Wise, if this is not the case;
+advocate, promote, or assist any unlawful act such as (by way of example only) copyright infringement or computer misuse.
+3. Suspension and termination
+We alone will determine whether there has been a breach of this Acceptable Use Policy through your use of our Services.
+
+3.1 We take breach of this policy seriously and may take the following actions:
+immediate, temporary, or permanent withdrawal of your right to use our Services;
+suspend or cancel your payment orders and take such other actions as we consider necessary;
+immediate, temporary, or permanent removal of any posting or material uploaded by you;
+issue of a warning;
+legal action against you including proceedings for reimbursement of all costs on an “all expenses” basis;
+reporting and disclosure of information to law enforcement authorities.
+4. Changes to the Acceptable Use Policy
+We may revise this Acceptable Use Policy at any time by amending this page. We recommend checking this page regularly as it is legally binding to you.
 const { PORT, NODE_ENV } = process.env
 const port = Number(PORT) || 4000
 
