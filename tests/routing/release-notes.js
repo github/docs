6d3288f704c0a5a0c6ab5b37453897b400022025
Peter Bengtsson <peterbe@github.com>
2023-04-20 07:43:09 -0400

    Exit releasenotes middleware early if page not found (#36497)

--- tests/routing/release-notes.js
+++ tests/routing/release-notes.js
@@ -59,8 +59,38 @@ describe('release notes', () => {
     expect(sectionTitleMatch).toBe(true)
   })
 
-  it('sends a 404 if a bogus version is requested', async () => {
+  it('404 if a bogus version is requested', async () => {
     const res = await get('/en/enterprise-server@12345/admin/release-notes')
     expect(res.statusCode).toBe(404)
+  })
+
+  it('404 if a the pathname only ends with the /release-notes', async () => {
+    // enterprise-server
+    {
+      const res = await get(`/en/enterprise-server@latest/ANY/release-notes`, {
+        followAllRedirects: true,
+      })
+      expect(res.statusCode).toBe(404)
+    }
+    // github-ae
+    {
+      const res = await get('/en/github-ae@latest/ANY/release-notes')
+      expect(res.statusCode).toBe(404)
+    }
+  })
+
+  it('404 if a the pathname only ends with the /admin', async () => {
+    // enterprise-server
+    {
+      const res = await get(`/en/enterprise-server@latest/ANY/admin`, {
+        followAllRedirects: true,
+      })
+      expect(res.statusCode).toBe(404)
+    }
+    // github-ae
+    {
+      const res = await get('/en/github-ae@latest/ANY/admin')
+      expect(res.statusCode).toBe(404)
+    }
   })
 })
