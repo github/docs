71759371a2c541288f575f7553da6b86d86dde35
Kevin Heis <heiskr@users.noreply.github.com>
2023-05-08 10:54:06 -0700

    Move more files into the search src directory (#36575)

--- src/search/components/SearchResults.tsx
+++ src/search/components/SearchResults.tsx
@@ -6,7 +6,7 @@ import cx from 'classnames'
 import type { SearchResultsT, SearchResultHitT } from './types'
 import { useTranslation } from 'components/hooks/useTranslation'
 import { Link } from 'components/Link'
-import { useQuery } from 'components/hooks/useQuery'
+import { useQuery } from 'src/search/components/useQuery'
 import { sendEvent, EventType } from 'src/events/components/events'
 
 import styles from './SearchResults.module.scss'
