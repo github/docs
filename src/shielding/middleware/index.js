dc54233c721f99fb6aaff5e12c86171022a511ee
Peter Bengtsson <peterbe@github.com>
2023-06-13 07:40:43 -0400

    Simple 404 response on old _next/data/ requests (#37756)

--- src/shielding/middleware/index.js
+++ src/shielding/middleware/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import handleInvalidQuerystrings from './handle-invalid-query-strings.js'
 import handleInvalidPaths from './handle-invalid-paths.js'
+import handleOldNextDataPaths from './handle-old-next-data-paths.js'
 import rateLimit from './rate-limit.js'
 
 const router = express.Router()
@@ -9,5 +10,6 @@ const router = express.Router()
 router.use(rateLimit)
 router.use(handleInvalidQuerystrings)
 router.use(handleInvalidPaths)
+router.use(handleOldNextDataPaths)
 
 export default router
