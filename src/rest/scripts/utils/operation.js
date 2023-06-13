5f7a717aaba69f6987b45714e9a8b14dc50cb851
Rachael Sewell <rachmari@github.com>
2023-06-13 15:27:45 -0700

    merge allOf JSON schema properties OpenAPI (#37821)

--- src/rest/scripts/utils/operation.js
+++ src/rest/scripts/utils/operation.js
@@ -4,6 +4,7 @@ import { readFile } from 'fs/promises'
 import { get, isPlainObject } from 'lodash-es'
 import { parseTemplate } from 'url-template'
 import path from 'path'
+import mergeAllOf from 'json-schema-merge-allof'
 
 import renderContent from '../../../../lib/render-content/index.js'
 import getCodeSamples from './create-rest-examples.js'
@@ -143,8 +144,9 @@ export default class Operation {
     if (this.#operation.operationId === 'checks/create') {
       delete schema.oneOf
     }
-
-    this.bodyParameters = isPlainObject(schema) ? await getBodyParams(schema, true) : []
+    // Merges any instances of allOf in the schema using a deep merge
+    const mergedAllofSchema = mergeAllOf(schema)
+    this.bodyParameters = isPlainObject(schema) ? await getBodyParams(mergedAllofSchema, true) : []
   }
 
   async renderPreviewNotes() {
