dec75c6fb238338982d57264e50a38518549bdba
Grace Park <gracepark@github.com>
2023-06-13 14:06:25 -0700

    markdownlint - Update to async and add severity (#37762)

--- src/content-linter/tests/unit/image-alt-text-end-punctuation.js
+++ src/content-linter/tests/unit/image-alt-text-end-punctuation.js
@@ -4,17 +4,15 @@ import markdownlint from 'markdownlint'
 import { imageAltTextEndPunctuation } from '../../lib/linting-rules/image-alt-text-end-punctuation.js'
 import { testOptions } from '../../lib/default-markdownlint-options.js'
 
+jest.setTimeout(60 * 1000)
+
 const fixtureFile = 'src/content-linter/tests/fixtures/image-alt-text-end-punctuation.md'
+const options = testOptions('MD112', imageAltTextEndPunctuation, fixtureFile)
+const result = await markdownlint.promises.markdownlint(options)
 
-describe('image alt text length rule', () => {
-  jest.setTimeout(60 * 1000)
-  const options = testOptions('MD112', imageAltTextEndPunctuation, fixtureFile)
-
-  const result = markdownlint.sync(options)
-  test('image alt text must have an end punctuation', () => {
-    const errors = result[fixtureFile]
-    expect(Object.keys(result).length).toBe(1)
-    expect(errors.length).toBe(2)
-    expect(errors.map((error) => error.lineNumber)).toEqual([3, 15])
-  })
+test('image alt text must have an end punctuation', () => {
+  const errors = result[fixtureFile]
+  expect(Object.keys(result).length).toBe(1)
+  expect(errors.length).toBe(2)
+  expect(errors.map((error) => error.lineNumber)).toEqual([3, 15])
 })
--- src/content-linter/tests/unit/image-alt-text-length.js
+++ src/content-linter/tests/unit/image-alt-text-length.js
@@ -4,17 +4,15 @@ import markdownlint from 'markdownlint'
 import { incorrectAltTextLength } from '../../lib/linting-rules/image-alt-text-length.js'
 import { testOptions } from '../../lib/default-markdownlint-options.js'
 
+jest.setTimeout(60 * 1000)
+
 const fixtureFile = 'src/content-linter/tests/fixtures/image-alt-text-length.md'
+const options = testOptions('MD111', incorrectAltTextLength, fixtureFile)
+const result = await markdownlint.promises.markdownlint(options)
 
-describe('image alt text length rule', () => {
-  jest.setTimeout(60 * 1000)
-  const options = testOptions('MD111', incorrectAltTextLength, fixtureFile)
-
-  const result = markdownlint.sync(options)
-  test('image with correct length alt text', () => {
-    const errors = result[fixtureFile]
-    expect(Object.keys(result).length).toBe(1)
-    expect(errors.length).toBe(2)
-    expect(errors.map((error) => error.lineNumber)).toEqual([1, 7])
-  })
+test('image with correct length alt text', () => {
+  const errors = result[fixtureFile]
+  expect(Object.keys(result).length).toBe(1)
+  expect(errors.length).toBe(2)
+  expect(errors.map((error) => error.lineNumber)).toEqual([1, 7])
 })
