10e02214af6b85db45316d099212b270552b0323
Tina Barfield <464067+tinabme@users.noreply.github.com>
2023-06-15 11:52:47 -0400

    Create &  migrate a subject folder for learning-tracks (#37890)
    Co-authored-by: Kevin Heis <heiskr@users.noreply.github.com>

--- src/learning-track/middleware/learning-track.js
+++ src/learning-track/middleware/learning-track.js
@@ -1,7 +1,7 @@
-import { getPathWithoutLanguage, getPathWithoutVersion } from '../lib/path-utils.js'
+import { getPathWithoutLanguage, getPathWithoutVersion } from '../../../lib/path-utils.js'
 import getLinkData from '../lib/get-link-data.js'
-import renderContent from '../lib/render-content/renderContent.js'
-import { getDeepDataByLanguage } from '../lib/get-data.js'
+import renderContent from '../../../lib/render-content/renderContent.js'
+import { getDeepDataByLanguage } from '../../../lib/get-data.js'
 
 export default async function learningTrack(req, res, next) {
   const noTrack = () => {
