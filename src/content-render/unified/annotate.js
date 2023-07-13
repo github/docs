3ab6f341fa438a400877ea9139f9156fdef670fb
Grace Park <gracepark@github.com>
2023-07-13 09:56:48 -0700

    Add error message if code annotation does not start with single line annotation (#39003)
    Co-authored-by: Kevin Heis <heiskr@users.noreply.github.com>

--- src/content-render/unified/annotate.js
+++ src/content-render/unified/annotate.js
@@ -77,13 +77,23 @@ function createAnnotatedNode(node) {
 
 function validate(lang, code) {
   if (!lang) {
-    throw new Error('No language specific for annotate info string')
+    throw new Error('No language specific for annotate info string.')
   }
   if (!languages[lang]) {
     throw new Error(
-      `Unsupported language for annotate info string. Please use one of: ${Object.keys(languages)}`,
+      `Unsupported language for annotate info string. Please use one of: ${Object.keys(
+        languages,
+      )}.`,
     )
   }
+
+  const firstLine = code.split('\n')[0]
+  if (!getRegexp(lang).test(firstLine)) {
+    throw new Error(
+      `Make sure the annotated code example starts with a single line annotation. It's currently starting with: ${firstLine}`,
+    )
+  }
+
   if (!new RegExp(getRegexp(lang), 'm').test(code)) {
     throw new Error(
       'Make sure the comment syntax matches the language. Use single-line comments only.',
