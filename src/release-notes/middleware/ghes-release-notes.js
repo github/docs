850f1eb7d67342668db2ec9eab8cbf1b3678c04a
Peter Bengtsson <peterbe@github.com>
2023-08-17 14:38:21 -0400

    For use of 'en' for all release-notes (#38975)

--- src/release-notes/middleware/ghes-release-notes.js
+++ src/release-notes/middleware/ghes-release-notes.js
@@ -8,7 +8,18 @@ export default async function ghesReleaseNotesContext(req, res, next) {
   const [requestedPlan, requestedRelease] = req.context.currentVersion.split('@')
   if (requestedPlan !== 'enterprise-server') return next()
 
-  const ghesReleaseNotes = getReleaseNotes('enterprise-server', req.language)
+  // (This applies to ghae release notes too)
+  // We deliberately force the language to be English for now.
+  // The underlying reason is that the content (in data/release-notes/**/*.yml)
+  // is Markdown that does NOT use variables controlled by English-only
+  // things like product names.
+  // Therefore, the **nouns (like product names) get translated** instead
+  // of left as is. For example. "Le GitHubbe Cöpilotte" instead
+  // of "GitHub Copilot".
+  // Until the Markdown sources used for release notes have Liquid
+  // variables (like `{% data variables.product.prodname_ghe_cloud %}}`)
+  // we'll force the text to be that from English.
+  const ghesReleaseNotes = getReleaseNotes('enterprise-server', 'en')
 
   // If the requested GHES release isn't found in data/release-notes/enterprise-server/*,
   // and it IS a valid GHES release, try being helpful and redirecting to the old location.
@@ -32,23 +43,35 @@ export default async function ghesReleaseNotesContext(req, res, next) {
     (r) => r.version === requestedRelease,
   ).patches
 
-  // Run the current release notes through the markdown rendering pipeline.
-  // Returns the current release's patches array: [{version, patchVersion, intro, date, sections}]
-  req.context.ghesReleaseNotes = await executeWithFallback(
-    req.context,
-    () => renderPatchNotes(currentReleaseNotes, req.context),
-    (enContext) => {
-      // Something in the release notes ultimately caused a Liquid
-      // rendering error. Let's start over and gather the English release
-      // notes instead.
-      const ghesReleaseNotes = getReleaseNotes('enterprise-server', 'en')
-      enContext.ghesReleases = formatReleases(ghesReleaseNotes)
-      const currentReleaseNotes = enContext.ghesReleases.find(
-        (r) => r.version === requestedRelease,
-      ).patches
-      return renderPatchNotes(currentReleaseNotes, enContext)
-    },
-  )
+  // This means the AUTOTITLE links are in the current language, but
+  // since we're already force the source of the release notes from English
+  // exclusively, by doing this we can force all reusables to be in English
+  // too and leave links as is.
+  const originalLanguage = req.context.currentLanguage
+  req.context.autotitleLanguage = originalLanguage
+  req.context.currentLanguage = 'en'
+
+  try {
+    // Run the current release notes through the markdown rendering pipeline.
+    // Returns the current release's patches array: [{version, patchVersion, intro, date, sections}]
+    req.context.ghesReleaseNotes = await executeWithFallback(
+      req.context,
+      () => renderPatchNotes(currentReleaseNotes, req.context),
+      (enContext) => {
+        // Something in the release notes ultimately caused a Liquid
+        // rendering error. Let's start over and gather the English release
+        // notes instead.
+        enContext.ghesReleases = formatReleases(ghesReleaseNotes)
+        const currentReleaseNotes = enContext.ghesReleases.find(
+          (r) => r.version === requestedRelease,
+        ).patches
+        return renderPatchNotes(currentReleaseNotes, enContext)
+      },
+    )
+  } finally {
+    // Restore the original language
+    req.context.currentLanguage = originalLanguage
+  }
 
   // GHES release notes on docs started with 2.20 but older release notes exist on enterprise.github.com.
   // So we want to use _all_ GHES versions when calculating next and previous releases.
