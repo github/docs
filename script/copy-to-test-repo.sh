2991405ec6cb07b512193a70d4696ee718721ed4
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-08-22 00:36:48 +0530

    Update copy-to-test-repo.sh

--- script/copy-to-test-repo.sh
+++ script/copy-to-test-repo.sh
@@ -1,43 +1,56 @@
 #!/bin/bash
 
-# Copies certain directories over to docs-internal-test and pushes. Useful for debugging actions
-# Doesn't copy over content/ and data/ directories
+# This script copies certain directories to a designated test repository and pushes the changes.
+# It is useful for debugging actions. It excludes specific directories like content/ and data/.
 
 echo "Make sure to run this script in the root path of docs-internal!"
 
+# Prompt the user for the relative path to the test repository.
 read -p "Relative path to test repo [../docs-internal-test] (enter for default):" TEST_PATH
 
+# If no input is provided, use the default relative path.
 TEST_PATH=${TEST_PATH:-../docs-internal-test}
 
-cd $TEST_PATH
-REPO_NAME=$(basename `git rev-parse --show-toplevel`)
+# Navigate to the test repository.
+cd "$TEST_PATH"
+
+# Get the name of the destination repository.
+REPO_NAME=$(basename "$(git rev-parse --show-toplevel)")
+
+# Get the current branch name.
 REPO_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+# Return to the original directory.
 cd -
 
-if [[ "$REPO_BRANCH" != "main" ]]; then
-  echo "docs-internal-test isn't on main branch"
+# Check if the current branch is the 'master' branch.
+if [[ "$REPO_BRANCH" != "master" ]]; then
+  echo "The current branch is not 'master'. Please switch to the 'master' branch."
   exit 1
-fi;
+fi
 
-if [[ "$REPO_NAME" == "docs-internal-test" ]]; then
-  echo "Copying files to $TEST_PATH..."
-  rsync -r --exclude='.git' --exclude='.gitattributes' --exclude='node_modules' --exclude='data' --exclude='content' --exclude=".github/CODEOWNERS" . $TEST_PATH
-  cd $TEST_PATH
-  if [[ `git status --porcelain` ]]; then
-    echo "Committing and pushing test files"
-    git add --all
-    git commit -m "testing (commited from script)"
-    git push -f
-  else
-    echo "No changes copied over. Are there relevent changes and are you pointing to the correct -test directory?"
-    exit 1
-  fi
-else
-  echo "$TEST_PATH is not the docs-internal-test repo directory"
+# Check if the destination repository name is 'docs-internal-test'.
+if [[ "$REPO_NAME" != "docs-internal-test" ]]; then
+  echo "The destination repository is not 'docs-internal-test'. Please adjust the repository name."
   exit 1
-fi;
+fi
+
+# Copy relevant files to the test repository.
+echo "Copying files to $TEST_PATH..."
+rsync -r --exclude='.git' --exclude='.gitattributes' --exclude='node_modules' --exclude='data' --exclude='content' --exclude=".github/CODEOWNERS" . "$TEST_PATH"
+
+# Navigate to the test repository.
+cd "$TEST_PATH"
+
+# Check if there are changes to commit.
+if [[ $(git status --porcelain) ]]; then
+  echo "Committing and pushing test files"
+  git add --all
+  git commit -m "Testing (committed from script)"
+  git push
+else
+  echo "No changes copied over. Please ensure there are relevant changes and you are pointing to the correct -test directory."
+  exit 1
+fi
 
 exit
-
-
-
