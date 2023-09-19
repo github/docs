f3a7ea9d5af207f20aac0fcf491fe4c4d63d98bf
Hector Alfaro <hectorsector@github.com>
2023-09-19 14:40:53 -0400

    Add playwright-axe test suite (#41814)

--- src/rest/components/RestCodeSamples.tsx
+++ src/rest/components/RestCodeSamples.tsx
@@ -248,7 +248,7 @@ export function RestCodeSamples({ operation, slug, heading }: Props) {
         </div>
         <div className="border-top d-inline-flex flex-justify-between width-full flex-items-center pt-2">
           <div className="d-inline-flex ml-2">
-            <TabNav aria-label="Example language selector">
+            <TabNav aria-label={`Example language selector for ${operation.title}`}>
               {languageSelectOptions.map((optionKey) => (
                 <TabNav.Link
                   key={optionKey}
@@ -300,6 +300,8 @@ export function RestCodeSamples({ operation, slug, heading }: Props) {
             `border-top rounded-1 my-0 ${getLanguageHighlight(selectedLanguage)}`,
           )}
           data-highlight={getLanguageHighlight(selectedLanguage)}
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+          tabIndex={0}
         >
           <code ref={requestCodeExample}>{displayedExample[selectedLanguage]}</code>
         </div>
@@ -314,7 +316,10 @@ export function RestCodeSamples({ operation, slug, heading }: Props) {
       ></h4>
       <div className="border rounded-1">
         {displayedExample.response.schema ? (
-          <TabNav className="pt-2 mx-2" aria-label="Example response format selector">
+          <TabNav
+            className="pt-2 mx-2"
+            aria-label={`Example response format selector for ${operation.title}`}
+          >
             {responseSelectOptions.map((optionKey) => (
               <TabNav.Link
                 key={optionKey}
@@ -353,6 +358,8 @@ export function RestCodeSamples({ operation, slug, heading }: Props) {
               )}
               data-highlight={'json'}
               style={{ maxHeight: responseMaxHeight }}
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+              tabIndex={0}
             >
               <code ref={responseCodeExample}>
                 {selectedResponse === ResponseKeys.example
