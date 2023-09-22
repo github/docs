717a42781f57411a52fc699dd401c91001ad3f2d
MOK13500 <143759764+MOK13500@users.noreply.github.com>
2023-09-21 21:13:46 -0500

    Revert "Repo sync"

--- src/content-linter/scripts/markdownlint.js
+++ src/content-linter/scripts/markdownlint.js
@@ -72,6 +72,8 @@ async function main() {
 
   // Initializes the config to pass to markdownlint based on the input options
   const { config, configuredRules } = getMarkdownLintConfig(errorsOnly, rules, customRules)
+  console.log(JSON.stringify(config, null, 2))
+  console.log(JSON.stringify(configuredRules, null, 2))
 
   // Run Markdownlint for content directory
   const resultContent = await markdownlint.promises.markdownlint({
