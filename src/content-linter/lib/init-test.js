4819fa2ec751fa0aa583663409b22274688eee11
Rachael Sewell <rachmari@github.com>
2023-09-22 11:12:49 -0700

    Add additional search-replace rules and tests (#42826)
    Co-authored-by: Peter Bengtsson <peterbe@github.com>

--- src/content-linter/lib/init-test.js
+++ src/content-linter/lib/init-test.js
@@ -2,10 +2,10 @@ import markdownlint from 'markdownlint'
 
 import { testOptions } from './default-markdownlint-options.js'
 
-export async function runRule(module, { strings, files } = {}) {
+export async function runRule(module, { strings, files, testConfig }) {
   if ((!strings && !files) || (strings && files))
     throw new Error('Must provide either Markdown strings or files to run a rule')
 
-  const options = testOptions(module.names[0], module, { strings, files })
+  const options = testOptions(module.names[0], module, { strings, files, testConfig })
   return await markdownlint.promises.markdownlint(options)
 }
