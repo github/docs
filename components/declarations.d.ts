81a101949f709eae160ce11d94be312e1c2f5e20
Mistiterane <139299484+Mistiterane@users.noreply.github.com>
2023-09-29 08:47:27 +0400

    Update declarations.d.ts

--- components/declarations.d.ts
+++ components/declarations.d.ts
@@ -1,6 +1,8 @@
 declare module '*.scss' {
   const content: Record<string, string>
   export default content
-}
+}"Authorization: Bearer TOKEN" \
+--request GET \
+"https://api.github.com/enterprises/avocado-corp/audit-log?phrase=action:pull_request+created:>=2022-01-01+actor:octocat"
 
 declare module 'highlightjs-curl'
