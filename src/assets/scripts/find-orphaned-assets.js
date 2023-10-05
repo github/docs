3da32a78fe02031fcc1496672b29e53503803307
Kevin Heis <heiskr@users.noreply.github.com>
2023-10-05 10:29:25 -0700

    Move stylesheets into src directory (#43727)

--- src/assets/scripts/find-orphaned-assets.js
+++ src/assets/scripts/find-orphaned-assets.js
@@ -95,7 +95,7 @@ async function main(opts) {
     }
   }
 
-  const roots = ['tests', 'components', 'script', 'stylesheets', 'contributing', 'src', 'assets']
+  const roots = ['tests', 'components', 'script', 'contributing', 'src', 'assets']
 
   for (const root of roots) {
     sourceFiles.push(
