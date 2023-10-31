7d17bc917eabeaec3a030d504c01dbc67e094cd3
Surav Shrestha <surav.shrestha@outlook.com>
2023-10-31 22:36:54 +0545

    docs: fix typos in src/content-linter (#29134)
    Co-authored-by: Jess Hosman <1183847+jhosman@users.noreply.github.com>

--- src/content-linter/style/github-docs.js
+++ src/content-linter/style/github-docs.js
@@ -192,7 +192,7 @@ export const searchReplaceConfig = {
         // Do not match developer.github.com/changes or
         // developer.github.com/enterprise/[0-9] or
         // developer.github.com/enterprise/{{something}} (e.g. liquid).
-        // There are occurences that will likely always remain in the content.
+        // There are occurrences that will likely always remain in the content.
         searchPattern: '/developer\\.github\\.com(?!\\/(changes|enterprise\\/([0-9]|{))).*/g',
         searchScope: 'all',
         severity: 'error',
