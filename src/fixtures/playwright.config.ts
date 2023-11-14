55cc45040be0c302742b3a2eb5e0817af4ac2cdc
Kevin Heis <heiskr@users.noreply.github.com>
2023-11-14 10:29:00 -0800

    Move playwright.config.ts to src/tests (#46130)

--- src/fixtures/playwright.config.ts
+++ src/fixtures/playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './src/fixtures/tests',
+  testDir: './tests',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
