7495bd6a5e6488d1126d50c8dbf2be8614da2187
Kevin Heis <heiskr@users.noreply.github.com>
2023-12-04 08:14:01 -0800

    Alerts that are the same as the monolith (#47012)
    Co-authored-by: Peter Bengtsson <peterbe@github.com>

--- src/fixtures/tests/markdown.js
+++ src/fixtures/tests/markdown.js
@@ -20,3 +20,30 @@ describe('markdown rendering', () => {
     expect(html).toMatch('HubGit Pages site')
   })
 })
+
+describe('alerts', () => {
+  test('basic rendering', async () => {
+    const $ = await getDOM('/get-started/markdown/alerts')
+    const alerts = $('#article-contents .ghd-alert')
+    // See src/fixtures/fixtures/content/get-started/markdown/alerts.md
+    // to be this confident in the assertions.
+    expect(alerts.length).toBe(5)
+    const svgs = $('svg', alerts)
+    expect(svgs.length).toBe(5)
+    const titles = $('.ghd-alert-title', alerts)
+      .map((_, el) => $(el).text())
+      .get()
+    expect(titles).toEqual(['Tip', 'Note', 'Important', 'Warning', 'Caution'])
+    const bodies = $('p:nth-child(2)', alerts)
+      .map((_, el) => $(el).text())
+      .get()
+      .map((s) => s.trim())
+    expect(bodies).toEqual([
+      "Here's a free tip",
+      'A note.',
+      'This is important',
+      'Just a warning',
+      'Be careful!',
+    ])
+  })
+})
