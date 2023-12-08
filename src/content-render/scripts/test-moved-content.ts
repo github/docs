35b26c6a14a8f0dec88c96fbaad2100d847e3093
Peter Bengtsson <peterbe@github.com>
2023-12-08 10:47:24 -0500

    Port test-moved-content.js to TypeScript (#47443)

--- src/content-render/scripts/test-moved-content.ts
+++ src/content-render/scripts/test-moved-content.ts
@@ -4,56 +4,58 @@ import path from 'path'
 
 import { program } from 'commander'
 
-import readFrontmatter from '#src/frame/lib/read-frontmatter.js'
+import readFrontmatter from 'src/frame/lib/read-frontmatter.js'
 
 const ROOT = process.env.ROOT || '.'
 const CONTENT_ROOT = path.resolve(path.join(ROOT, 'content'))
 
 program
   .description('Test that a file correctly got moved')
-  .arguments('old', 'old file or folder name')
-  .arguments('new', 'new file or folder name')
+  .arguments('old')
+  .arguments('new')
   .parse(process.argv)
 
-main(program.args)
+main([program.args[0], program.args[1]])
 
-async function main(nameTuple) {
+async function main(nameTuple: [string, string]) {
   const [before, after] = nameTuple
   assert(!fs.existsSync(before), `File or folder ${before} exists`)
   assert(fs.existsSync(after), `File or folder ${after} exists`)
   if (after.endsWith('.md')) {
     const fileContent = fs.readFileSync(after, 'utf-8')
-    const { data } = readFrontmatter(fileContent)
+    const fm = readFrontmatter(fileContent)
+    const { data } = fm
     const oldHref = makeHref(CONTENT_ROOT, before)
-    assert(data.redirect_from.includes(oldHref), `Redirect from ${oldHref} not found`)
+    if (data) assert(data.redirect_from.includes(oldHref), `Redirect from ${oldHref} not found`)
     {
       const parentIndexMd = path.join(path.dirname(after), 'index.md')
       const fileContent = fs.readFileSync(parentIndexMd, 'utf-8')
       const { data } = readFrontmatter(fileContent)
       const afterShortname = '/' + after.split('/').slice(-1)[0].replace(/\.md$/, '')
-      assert(data.children.includes(afterShortname), `Child ${afterShortname} not found`)
+      if (data) assert(data.children.includes(afterShortname), `Child ${afterShortname} not found`)
     }
   } else {
     const fileContent = fs.readFileSync(path.join(after, 'index.md'), 'utf-8')
     const { data } = readFrontmatter(fileContent)
     const oldHref = makeHref(CONTENT_ROOT, before)
-    assert(data.redirect_from.includes(oldHref), `Redirect from ${oldHref} not found`)
+    if (data) assert(data.redirect_from.includes(oldHref), `Redirect from ${oldHref} not found`)
     {
       const parentIndexMd = path.join(path.dirname(after), 'index.md')
       const fileContent = fs.readFileSync(parentIndexMd, 'utf-8')
       const { data } = readFrontmatter(fileContent)
       const afterShortname = '/' + after.split('/').slice(-1)
-      assert(data.children.includes(afterShortname), `Child ${afterShortname} not found`)
+      if (data) assert(data.children.includes(afterShortname), `Child ${afterShortname} not found`)
     }
   }
 }
 
-function makeHref(root, filePath) {
+function makeHref(root: string, filePath: string) {
   const nameSplit = path.relative(root, filePath).split(path.sep)
   if (nameSplit.slice(-1)[0] === 'index.md') {
     nameSplit.pop()
   } else {
-    nameSplit.push(nameSplit.pop().replace(/\.md$/, ''))
+    const last = nameSplit.pop()
+    if (last) nameSplit.push(last.replace(/\.md$/, ''))
   }
   return '/' + nameSplit.join('/')
 }
