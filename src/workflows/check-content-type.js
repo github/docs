81a4ba534c6af57146a3ed9ad8f0ef015269f511
Rachael Sewell <rachmari@github.com>
2023-12-19 14:36:03 -0800

    Codeowners legal workflow update (#48150)

--- src/workflows/check-content-type.js
+++ src/workflows/check-content-type.js
@@ -11,6 +11,8 @@ main()
 async function main() {
   const filePaths = JSON.parse(FILE_PATHS_CONTENT_TYPES)
   const containsRai = checkContentType(filePaths, CONTENT_TYPE)
+  console.log('filePaths', filePaths)
+  console.log('containsRai', containsRai)
   if (containsRai.length === 0) {
     coreLib.setOutput('contentType', false)
   } else {
