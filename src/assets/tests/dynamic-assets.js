70d745165b6dd5a2f7c9550415350edbf44ecf84
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-20 19:35:24 -0500

    spelling: set
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/assets/tests/dynamic-assets.js
+++ src/assets/tests/dynamic-assets.js
@@ -58,7 +58,7 @@ describe('dynamic assets', () => {
     expect(height).toBe(448)
   })
 
-  test("can't set set max-width indicator on the PNG that is already on disk", async () => {
+  test("can't set max-width indicator on the PNG that is already on disk", async () => {
     const res = await get(`/assets/images/mw-1000/_fixtures/screenshot.png`)
     expect(res.statusCode).toBe(404)
   })
