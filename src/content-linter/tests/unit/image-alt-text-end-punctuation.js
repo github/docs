4ad0760a4e3b9c3a3a44174ace66de8a7f1e6076
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-23 22:25:42 -0500

    spelling: punctuation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/content-linter/tests/unit/image-alt-text-end-punctuation.js
+++ src/content-linter/tests/unit/image-alt-text-end-punctuation.js
@@ -2,7 +2,7 @@ import { runRule } from '../../lib/init-test.js'
 import { imageAltTextEndPunctuation } from '../../lib/linting-rules/image-alt-text-end-punctuation.js'
 
 describe(imageAltTextEndPunctuation.names.join(' - '), () => {
-  test('image alt text without end punctutation errors', async () => {
+  test('image alt text without end punctuation errors', async () => {
     const markdown = [
       '# Heading',
       '',
@@ -29,7 +29,7 @@ describe(imageAltTextEndPunctuation.names.join(' - '), () => {
       insertText: '.',
     })
   })
-  test('image alt text with end punctutation passes', async () => {
+  test('image alt text with end punctuation passes', async () => {
     const markdown = [
       '# Heading',
       '',
