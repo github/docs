ec4d85c7d35c29c1e8a9be2fe9717580de6f4d73
Kevin Heis <heiskr@users.noreply.github.com>
2024-01-02 13:38:14 -0800

    Migrate Callout React component to Alert (#47226)
    Co-authored-by: Peter Bengtsson <peterbe@github.com>

--- src/fixtures/tests/alerts.js
+++ src/fixtures/tests/alerts.js
@@ -1,9 +1,9 @@
 import { getDOM } from '#src/tests/helpers/e2etest.js'
 
-describe('callouts', () => {
+describe('alerts', () => {
   test('article page', async () => {
     const $ = await getDOM('/get-started/foo/page-with-callout')
-    const callout = $('[data-testid=callout]')
+    const callout = $('[data-testid=alert] div')
     expect(callout.html()).toBe('<p>Callout for HubGit Pages</p>')
   })
 
@@ -11,16 +11,16 @@ describe('callouts', () => {
     // This page has `product:` property which is a piece of Liquid
     // which makes it so that the rendered output of that becomes
     // an empty string.
-    // This test tests that callout is not rendered if its output
+    // This test tests that alert is not rendered if its output
     // "exits" but is empty.
     const $ = await getDOM('/enterprise-server@latest/get-started/foo/page-with-callout')
-    const callout = $('[data-testid=callout]')
+    const callout = $('[data-testid=alert]')
     expect(callout.length).toBe(0)
   })
 
   test('toc landing page', async () => {
     const $ = await getDOM('/actions/category')
-    const callout = $('[data-testid=callout]')
+    const callout = $('[data-testid=alert] div')
     expect(callout.html()).toBe('<p>This is the callout text</p>')
   })
 })
