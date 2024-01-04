454d528a7f1dc35d54b90e9e19f4627e1a8ce862
Isabel Schöps Thiel @IsabelSchoepd <155141998+IST-Github@users.noreply.github.com>
2024-01-04 09:58:03 +0100

    Delete src/bookmarklets directory

--- src/bookmarklets/view-in-development.js
+++
@@ -1,1 +0,0 @@
-javascript:(()=>{window.location.href=window.location.href.replace(/https:\/\/docs\.github\.com/,'http://localhost:4000').replace(/https:\/\/.*\..*\.(com|net|dev|io|org|ms)/,'http://localhost:4000')})()
