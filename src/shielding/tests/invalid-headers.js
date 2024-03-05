34685538bf1f8fa4625b4958482aa6ff50c4ff22
Peter Bengtsson <peterbe@github.com>
2024-03-05 13:43:39 -0500

    Drop cache control from invalid header shielding (#49528)

--- src/shielding/tests/invalid-headers.js
+++ src/shielding/tests/invalid-headers.js
@@ -4,13 +4,9 @@ describe('invalid headers', () => {
   test('400 if containing x-invoke-status (instead of redirecting)', async () => {
     const res = await get('/', { headers: { 'x-invoke-status': '203' } })
     expect(res.statusCode).toBe(400)
-    expect(res.headers['cache-control']).toMatch('public')
-    expect(res.headers['cache-control']).toMatch(/max-age=[1-9]/)
   })
   test('400 if containing x-invoke-status (instead of 200)', async () => {
     const res = await get('/en', { headers: { 'x-invoke-status': '203' } })
     expect(res.statusCode).toBe(400)
-    expect(res.headers['cache-control']).toMatch('public')
-    expect(res.headers['cache-control']).toMatch(/max-age=[1-9]/)
   })
 })
