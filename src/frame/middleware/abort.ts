a76f6b85a912135c6e5ddc22ba3d063dcccce296
Peter Bengtsson <peterbe@github.com>
2024-05-08 07:17:36 -0400

    Port `abort.js` to TypeScript (#50498)

--- src/frame/middleware/abort.ts
+++ src/frame/middleware/abort.ts
@@ -1,6 +1,19 @@
-import statsd from '#src/observability/lib/statsd.js'
+import type { NextFunction, Response } from 'express'
 
-export default function abort(req, res, next) {
+import statsd from '#src/observability/lib/statsd.js'
+import { ExtendedRequest } from '@/types'
+
+class AbortError extends Error {
+  statusCode: number
+  code: string
+  constructor(message: string, statusCode: number, code: string) {
+    super(message)
+    this.statusCode = statusCode
+    this.code = code
+  }
+}
+
+export default function abort(req: ExtendedRequest, res: Response, next: NextFunction) {
   // If the client aborts the connection, send an error
   req.once('aborted', () => {
     // ignore aborts from next, usually has to do with webpack-hmr
@@ -21,9 +34,7 @@ export default function abort(req, res, next) {
     }
     statsd.increment('middleware.abort', 1, incrementTags)
 
-    const abortError = new Error('Client closed request')
-    abortError.statusCode = 499
-    abortError.code = 'ECONNRESET'
+    const abortError = new AbortError('Client closed request', 499, 'ECONNRESET')
 
     // Pass the error to the Express error handler
     return next(abortError)
