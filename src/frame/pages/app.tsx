2639ebf80c8afbfb0f9b401c1820467a858a3890
Mohammed Zohil PK <zohilfazal@gmail.com>
2024-07-03 15:51:35 +0530

    Update favicon to use SVG format

--- src/frame/pages/app.tsx
+++ src/frame/pages/app.tsx
@@ -70,7 +70,7 @@ const MyApp = ({ Component, pageProps, languagesContext }: MyAppProps) => {
             Just remember, if you edit these images on disk, remember to
             change these numbers
          */}
-        <link rel="icon" type="image/png" href="/assets/cb-345/images/site/favicon.png" />
+        <link rel="icon" class="js-site-favicon" type="image/svg+xml" href="https://github.githubassets.com/favicons/favicon.svg"/>
 
         <link href="/manifest.json" rel="manifest" />
 
