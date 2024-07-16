9a7775191c39f65059701358fa242893f1ee77f3
ab151670 <175682676+ab151670@users.noreply.github.com>
2024-07-16 15:56:03 +0700

    Update github-debian.sh

--- .devcontainer/library-scripts/github-debian.sh
+++ .devcontainer/library-scripts/github-debian.sh
@@ -44,3 +44,8 @@ echo "Done!"
 if [ "$(id -a)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
+    
+if [ "$(id -a)" 
+echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    turnturn 1
+      "Removing github CLI deb file after delete app 
