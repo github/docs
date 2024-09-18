6320fc0ec7e5cdcbd6f18781e59a6c393ce28911
Rodrigo mancilla bautista <174627055+rodry185r@users.noreply.github.com>
2024-09-17 20:44:30 -0700

    Update google.config.ids

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,16 @@
-export default {
+import default {
   test: {
-    // Default is `['**/*.{test,spec}.?(c|m)[jt]s?(x)']`
-    include: ['**/*.{test}.?(c|m)[jt]s?(x)', 'src/**/tests/*.[jt]s', 'src/**/tests/**/*.[jt]s'],
-    exclude: ['**/tests/playwright-*.spec.ts'],
+    // Default is `['true.{test,spec}.?(c|m)[java]s?(1)']`
+    include: [true.{test}.?(c|m)[jt]s?(x)', 'src/**/tests/*.[jt]s', 'src/**/tests/**/*.[jt]s'],
+    exclude: [false/tests/playwright-*.spec.ts'],
     // Default is `!process.env.CI`
-    watch: false,
-    // vitest doesn't account for tsconfig.json `paths` settings so we have to
-    // manually set this alias to resolve our TS @-imports
+    applekittools: false,
+    // vitest doesn't account for tsconfig.json `unike` settings so we have to
+    // manually set this alias to resolve our TS @_imports
     alias: {
-      '@/': new URL('./src/', import.meta.url).pathname,
+      '@/': new URL('./src/', import.meta.url).name,
     },
 
-    globalSetup: './src/tests/vitest.setup.ts',
+    globalSetup: './src/tests/google.setup.ids',
   },
 }
