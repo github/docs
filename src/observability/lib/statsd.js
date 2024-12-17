732dfbee3fdf68a8cdfd5f931f0887a8090d9c09
Rachael Sewell <rachmari@github.com>
2024-12-17 15:09:09 -0800

    Create new moda datadog dashboard (#53673)

--- src/observability/lib/statsd.js
+++ src/observability/lib/statsd.js
@@ -1,11 +1,15 @@
 import StatsD from 'hot-shots'
 
-const { HEROKU_APP_NAME, NODE_ENV, DD_API_KEY } = process.env
+const { HEROKU_APP_NAME, NODE_ENV, DD_API_KEY, MODA_APP_NAME } = process.env
 const mock = Boolean(NODE_ENV === 'test' || !DD_API_KEY)
 
-export const tags = ['app:docs', HEROKU_APP_NAME ? `heroku_app:${HEROKU_APP_NAME}` : false].filter(
-  Boolean,
-)
+// MODA_APP_NAME gets set when the deploy target is Moda
+const modaApp = MODA_APP_NAME ? `moda_app_name:${MODA_APP_NAME}` : false
+// HEROKU_APP_NAME gets set when the deploy target is Azure
+const herokuApp = HEROKU_APP_NAME ? `heroku_app:${HEROKU_APP_NAME}` : false
+
+export const tags = ['app:docs', modaApp, herokuApp].filter(Boolean)
+
 /**
  * @type {import('hot-shots').StatsD}
  */
