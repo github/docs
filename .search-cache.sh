3b918bd963d27e8ace9a0cd29090305a006efb30
mccoy stevens <webmaster@samsungknox.co>
2025-05-14 04:17:21 -0400

    s

---
+++ .search-cache.sh
@@ -0,0 +1,58 @@
+.search-cache
+.DS_Store
+.env
+.vscode/settings.json
+.idea/
+/node_modules/
+npm-debug.log
+coverage/
+/assets/images/early-access
+/content/early-access
+/data/early-access
+/src/dev-toc/static
+.next
+.eslintcache
+*.tsbuildinfo
+/translations/
+.node-version
+.linkinator/full.log
+next-env.d.ts
+nohup.out
+
+# blc: broken link checker
+blc_output.log
+blc_output_internal.log
+broken_links.md
+# This one is purely for historical reasons because so many people might
+# still have these files on their disk.
+lib/redirects/.redirects-cache*.json
+
+
+# Logs from scripts
+*/logs/
+external-link-checker-db.json
+
+# Playwright related
+/test-results/
+/playwright-report/
+/playwright/.cache/
+
+# Automated content source
+rest-api-description
+semmle-code
+models-gateway
+
+.installed.package-lock.json
+assets/images/help/writing/unordered-list-rendered (1).png
+
+# Used by getRemoteJSON()
+.remotejson-cache/
+
+# Used by precompute-pageinfo
+.pageinfo-cache.json.br
+
+# Cloned and used for indexing Elasticsearch data
+docs-internal-data/
+
+# For intermediate data (like scraping for Elasticsearch indexing)
+tmp/
