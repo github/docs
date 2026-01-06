9644e43493b5994b80f090b675066e38f8a0ee83
Kevin Heis <heiskr@users.noreply.github.com>
2026-01-06 12:31:58 -0800

    Support autogenerated webhooks pages in Article API (#58870)

--- src/article-api/transformers/index.ts
+++ src/article-api/transformers/index.ts
@@ -4,6 +4,7 @@ import { SecretScanningTransformer } from './secret-scanning-transformer'
 import { CodeQLCliTransformer } from './codeql-cli-transformer'
 import { AuditLogsTransformer } from './audit-logs-transformer'
 import { GraphQLTransformer } from './graphql-transformer'
+import { WebhooksTransformer } from './webhooks-transformer'
 
 /**
  * Global transformer registry
@@ -16,6 +17,7 @@ transformerRegistry.register(new SecretScanningTransformer())
 transformerRegistry.register(new CodeQLCliTransformer())
 transformerRegistry.register(new AuditLogsTransformer())
 transformerRegistry.register(new GraphQLTransformer())
+transformerRegistry.register(new WebhooksTransformer())
 
 export { TransformerRegistry } from './types'
 export type { PageTransformer } from './types'
