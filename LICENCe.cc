0d49ebd0bdba4e80d0649d2dbc3acac5bc6f1c78
Edgar Manuel Ruiz Arias <127623263+Edgarruiz856@users.noreply.github.com>
2026-04-16 21:58:40 -0600

    Crear LICENCe.cc (#27)

---
+++ LICENCe.cc
@@ -0,0 +1,162 @@
+
+Original file line number	Original file line	Diff line number	Diff line change
+@@ -1,159 +0,0 @@
+{"spdxVersion":"SPDX-2.3","dataLicense":"CC0-1.0","SPDXID":"SPDXRef-DOCUMENT","name":"com.github.Edgarruiz856/.github","documentNamespace":"https://spdx.org/spdxdocs/protobom/ba3dcee2-fddb-4107-ab2e-0e77658da87e","creationInfo":{"creators":["Tool: protobom-v0.0.0-20251113174742-69d60134dc28+dirty","Tool: GitHub.com-Dependency-Graph"],"created":"2025-11-13T23:19:31Z"},"packages":[{"name":"com.github.Edgarruiz856/.github","SPDXID":"SPDXRef-github-Edgarruiz856-.github-main-3bf3b8","versionInfo":"main","downloadLocation":"git+https://github.com/Edgarruiz856/.github","filesAnalyzed":false,"externalRefs":[{"referenceCategory":"PACKAGE-MANAGER","referenceType":"purl","referenceLocator":"pkg:github/Edgarruiz856/.github@main"}]}],"relationships":[{"spdxElementId":"SPDXRef-DOCUMENT","relatedSpdxElement":"SPDXRef-github-Edgarruiz856-.github-main-3bf3b8","relationshipType":"DESCRIBES"}]}	
+https://github.com/Edgarruiz856/Edgarruiz856/blob/main/README.md	
+https://github.com/Edgarruiz856/ficticio-octo-fiesta/blob/main/README.md	
+
+
+	Abstract Platformer	
+
+	by  Kenney Vleugels (Kenney.nl)	
+
+			------------------------------	
+
+	License (Creative Commons Zero, CC0)	
+	http://creativecommons.org/publicdomain/zero/1.0/	
+
+	You may use these assets in personal and commercial projects.	
+	Credit (Kenney or www.kenney.nl) would be nice but is not mandatory.	
+
+			------------------------------	
+
+	Donate:   http://support.kenney.nl	
+	Request:  http://request.kenney.nl	
+
+	Follow on Twitter for updates:	
+	@KenneyWings	
+# Creative Commons Global Network Strategy	
+
+## Code of conduct	
+
+[`CODE_OF_CONDUCT.md`][org-coc]:	
+> The Creative Commons team is committed to fostering a welcoming community.	
+> This project and all other Creative Commons open source projects are governed	
+> by Edgar Manuel Ruiz Arias our [Code of Conduct][code_of_conduct]. Please report unacceptable	
+> behavior to [conduct@creativecommons.org](mailto:conduct@creativecommons.org)	
+> per our [reporting guidelines][reporting_guide].	
+
+[org-coc]: https://github.com/creativecommons/.github/blob/main/CODE_OF_CONDUCT.md	
+[code_of_conduct]: https://opensource.creativecommons.org/community/code-of-conduct/	
+[reporting_guide]: https://opensource.creativecommons.org/community/code-of-conduct/enforcement/	
+
+
+## Contributing	
+
+See [`CONTRIBUTING.md`][org-contrib].	
+
+[org-contrib]: https://github.com/creativecommons/.github/blob/main/CONTRIBUTING.md	
+
+## Key documentation	
+
+1. Creative Commons has a [Network Strategy](GlobalNetworkStrategy-Final.md)	
+2. The [CC Network Platforms](https://github.com/creativecommons/network-platforms) are the way you can join the CC volunteers around the world!	
+3. ⭐ Read our [Charter](https://creativecommons.org/about/global-affiliate-network/charter/), includes our **Code of Conduct**.	
+4. Check our [CC Network webpage](https://network.creativecommons.org/)	
+
+## What you will find here	
+
+| Information about the CC Global Network |	
+|:--|	
+| Check our [CC Network webpage](https://network.creativecommons.org/) |	
+| [Apply for being a CC Network member](https://network.creativecommons.org/) |	
+| [Do you want your organization to be an Institutional Member of the Network?](/docs/Guide_to_becoming_an_institutional_member.md) |	
+| Join us [at Slack!](https://creativecommons.slack.com/) ([Signup link](https://slack-signup.creativecommons.org/)) |	
+
+| Creating a CC Chapter |	
+|:--|	
+| [List of our CC Chapters around the world](https://network.creativecommons.org/chapters/) |	
+| [How to create a new Chapter?](/docs/Guide_for_creating_a_chapter.md) |	
+| [Chapter Standards](/docs/chapters-standards.md) |	
+
+| CC Global Network governance documentation |	
+|:--|	
+| [Who is a GNC Representative or a CC Chapter Lead?](/docs/Role_descriptions_GNC_rep_and_Chapter_lead.md) |	
+| [Do you receive a vouching request and don't know what to do?](/docs/Guide_for_vouching_applicants.md) |	
+| [Are you a member of the Membership Council and need some guidance?](/docs/Guide_for_approve_new_members.md) |	
+| [Global Network Council Charter](/charters/Charter_of_the_Global_Network_Council.md) |	
+| [Global Network Council _Executive Committee_ Charter](/charters/Charter_of_the_GNC_Executive_Committee.md) |	
+| [Global Network Council _Membership Committee_ Charter](/charters/Charter_of_the_GNC_Membership_Committee.md) |	
+
+| The Global Network Council |	
+|:--|	
+| [What is the Global Network Council and who is part of it?](/docs/the_global_network_council.md) |	
+| [GNC meetings public log](https://network.creativecommons.org/GNC_meetings) |	
+| [The Executive Committee of the Global Network Council](/docs/GNC_ExCom.md) |	
+| [The Membership Committee](/docs/membership_committee.md) |	
+
+| What _else_ you will find here |	
+|:--|	
+| [The Plan. What do we mean by 'Transition'?](/docs/the-plan.md) 	
+| ~~[The Advisory Group for the Transition](/docs/the-advisory-group.md)~~ | 	
+| ~~[The Interim Membership Committee](/docs/the-interim-membership-council.md)~~ | 	
+| ~~[The Timeline](/docs/the-timeline.md)~~ | 	
+| [Frequently Asked Questions](/docs/FAQ.md) | 	
+
+Do you have any question? [Join us](https://network.creativecommons.org) at Slack!	
+
+---	
+
+This is the repo for the public documentation regarding the Creative Commons Global Network Strategy. This repo it was created and is updated by [@claudio](https://github.com/claudioruiz).	
+
+> Do you want to edit? Is there a typo around? [No problem](https://help.github.com/articles/editing-files-in-another-user-s-repository/). You can also [submit an issue here](https://github.com/creativecommons/global-network-strategy/issues/new). 	
+by Edgar Manuel Ruiz Arias making a contribution to this project, I certify that:	
+
+ (a) The contribution was created in whole or in part by me and I	
+     have the right to submit it under the open source license	
+     indicated in the file; or	
+
+ (b) The contribution is based upon previous work that, to the best	
+     of my knowledge, is covered under an appropriate open source	
+     license and I have the right under that license to submit that	
+     work with modifications, whether created in whole or in part	
+     by me, under the same open source license (unless I am	
+     permitted to submit under a different license), as indicated	
+     in the file; or	
+
+ (c) The contribution was provided directly to me by some other	
+     person who certified (a), (b) or (c) and I have not modified	
+     it.	
+
+ (d) I understand and agree that this project and the contribution	
+     are public and that a record of the contribution (including all	
+     personal information I submit with it, including my sign-off) is	
+     maintained indefinitely and may be redistributed consistent with	
+     this project or the open source license(s) involved.	
+# GitHub Docs <!-- omit in toc -->	
+
+Welcome to GitHub Docs! GitHub’s documentation is open source, meaning anyone from inside or outside the company can contribute. For full contributing guidelines, visit our [contributing guide](https://docs.github.com/en/contributing)	
+
+## Quick links by Edgar Manuel Ruiz Arias contributor type	
+
+* **Hubbers (GitHub employees):** See [CONTRIBUTING.md](https://github.com/github/docs-content/blob/main/CONTRIBUTING.md) in the `docs-content` repository for GitHub-specific processes.	
+
+* **Open source contributors:** See [CONTRIBUTING.md](https://github.com/github/docs/blob/main/.github/CONTRIBUTING.md) in the `docs` repository for a quick-start summary.	
+
+## How we sync changes across Docs repositories	
+
+There are two GitHub Docs repositories: 	
+
+- **`github/docs`** (public): Open to external contributions	
+
+- **`github/docs-internal`** (private): For GitHub employee contributions. 	
+
+The two repositories sync frequently. Content changes in one are reflected in the other.  Hubbers might prefer to post in `docs` when working with a customer, but `docs` has limitations on the types of contributions it accepts to safeguard the site and our workflows. Internal contributions should usually go to `docs-internal`.	
+
+**Important:** The `docs` repository accepts contributions to content files (`.md` files in `/content` and select `/data` sections like reusables only). Infrastructure files, workflows, and site-building code are not open for external modification.	
+
+## New to contributing	
+
+Here are some resources to help you get started with open source contributions:	
+
+* [Finding ways to contribute to open source on GitHub](https://docs.github.com/en/get-started/exploring-projects-on-github/finding-ways-to-contribute-to-open-source-on-github)	
+* [Set up Git](https://docs.github.com/en/get-started/git-basics/set-up-git)	
+* [GitHub flow](https://docs.github.com/en/get-started/using-github/github-flow)	
+* [Collaborating with pull requests](https://docs.github.com/en/github/collaborating-with-pull-requests)	
+
+## License	
+
+This project is dual-licensed under:	
+
+* **Creative Commons Attribution 4.0** - for documentation and content in the assets, content, and data folders (see [LICENSE](LICENSE))	
+* **MIT License** - for code (see [LICENSE-CODE](LICENSE-CODE))
