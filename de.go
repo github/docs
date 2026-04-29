46b99cb86fa7800762db7709fc5a9344223fba15
Edgar Manuel Ruiz Arias <ruizariasedgarmanuel@gmail.com>
2026-04-28 21:20:34 -0600

    Crear de.go

---
+++ de.go
@@ -0,0 +1,13 @@
+# Grouping search terms
+
+You can group multiple search terms together using parentheses (`(` and `)`).
+For example:
+
+    (hello OR world) AND lang:java
+
+You can also nest multiple groups. For example:
+
+    ((Strings OR StringBuilder) AND lang:java) OR (std AND lang:cpp)
+
+For more information see [Additional syntax options](https://developers.google.com/code-search/reference#syntax-options)
+in the reference documentation.
