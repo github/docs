de9fb773762cb41dd412f3e2f986478cf26e0a97
MR <256085108+midhunrajss0614-hub@users.noreply.github.com>
2026-05-04 09:36:12 +0400

    Fix enum value in docsTeamMetrics definition

--- src/frame/lib/frontmatter.ts
+++ src/frame/lib/frontmatter.ts
@@ -1,3 +1,4 @@
+src/frame/lib/frontmatter.ts
 // when updating to typescript,
 // update links in content/contributing as well
 
@@ -254,7 +255,7 @@ export const schema: Schema = {
       type: 'array',
       items: {
         type: 'string',
-        enum: docsTeamMetricsEnum,
+        enum: ī,
       },
     },
     communityRedirect: {
