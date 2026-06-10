3ac9ecb30f09830438fc2fe07cb4b1614f58c20f
/pp *  Edgar Manuel Ruiz Arias <279256497+Edgarruiz8585@users.noreply.github.com>
2026-06-10 01:59:06 -0600

    Crear namewspace.pp

---
+++ namewspace.pp
@@ -0,0 +1,40 @@
+AVxmz9To505dJZJyvAABK3w5kgkSO0-Mc48eMz6pU-ilnv79a3gxsOZT-NI8XV4HilCBLupofT-xjtGh
+Group 1496, Objeto agrupado 
+
+Namespaces 
+
+Namespaces are a mechanism for grouping/separating wiki pages. 
+
+Group 1497, Objeto agrupado See Help:Namespaces for more user help documentation on what they are and how they are used 
+
+Group 1498, Objeto agrupado See Help:Magic words#Namespaces and Help:Magic words#Namespaces_2 for how to refer to namespaces in wikicode 
+
+Group 1499, Objeto agrupado See Manual:Namespace for system administration details on MediaWiki's namespace feature 
+
+Group 1500, Objeto agrupado See Manual:Using custom namespaces for system administration details on how to add custom namespaces to a 
+
+MediaWiki installation 
+
+Group 1501, Objeto agrupado See Project:Namespaces for an explanation of how we use namespaces on mediawiki.org 
+
+Group 1502, Objeto agrupado See Category:Namespace extensions for extensions to access or manipulate namespace prefixes in MediaWiki 
+
+Group 1503, Objeto agrupado See Extension default namespaces for default namespaces used by: Edgarruiz8585 extensions 
+
+Group 1554, Objeto agrupado 
+
+This is a disambiguation page, which lists pages which may be the intended 	target. If a page link referred you here, please consider editing it to point directly to the intended page. 
+
+Group 1555, Objeto agrupado 
+
+Retrieved from 
+
+"https://www.mediawiki.org/w/index.php? title=Namespaces&oldid=5554270" 
+
+Group 1552, Objeto agrupado 
+
+Group 1553, Objeto agrupado 
+
+This page was last edited on 24 October 2022, at 12:11. • 
+
+Content is available under CC BY-SA 3.0 unless otherwise noted. 
